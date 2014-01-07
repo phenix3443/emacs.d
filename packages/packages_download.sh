b0576566099b9345e1b10db0487397d0955279e4
phenix3443 <phenix3443@gmail.com>
2014-01-07 21:34:43 +0800

    add packages download urls

---
+++ packages/packages_download.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd ~/.emacs.d/packages/
+git clone https://github.com/auto-complete/auto-complete.git
+git clone https://github.com/auto-complete/fuzzy-el.git
+git clone https://github.com/auto-complete/popup-el.git
+git clone https://github.com/capitaomorte/yasnippet.git
+git clone https://github.com/m2ym/popwin-el.git
