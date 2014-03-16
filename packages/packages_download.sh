4f795e729bcf2432527849c01d86c3a7528e1d6f
phenix3443 <phenix3443@gmail.com>
2014-03-16 15:27:25 +0800

    1.添加lua-mode

--- packages/packages_download.sh
+++ packages/packages_download.sh
@@ -6,3 +6,4 @@ git clone https://github.com/auto-complete/fuzzy-el.git
 git clone https://github.com/auto-complete/popup-el.git
 git clone https://github.com/capitaomorte/yasnippet.git
 git clone https://github.com/m2ym/popwin-el.git
+git clone https://github.com/immerrr/lua-mode.git
