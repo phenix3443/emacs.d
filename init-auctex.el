f881612a4c1234f278d909ab8e106ffb7e12c189
phenix3443 <phenix3443@gmail.com>
2014-07-14 00:54:58 +0800

    config auctex

---
+++ init-auctex.el
@@ -0,0 +1,30 @@
+;;Time-stamp: <2014-07-14 00:50:37 phenix>
+;;;default from auctex-manual 
+;;; 1.3  5.3
+(setq TeX-auto-save t)
+
+
+;;;2.5 Entering Mathematics
+(add-hook 'LaTeX-mode-hook 'LaTex-math-mode)
+;;;2.6 Completion
+
+;;; 2.10 Filling 
+(add-hook 'LaTeX-mode-hook 'turn-on-auto-fill)
+;;; 3.2 Folding
+(add-hook 'LaTeX-mode-hook (lambda ()
+							  (TeX-fold-mode 1)))
+;;;4.1.3 TeX-engine
+(setq TeX-engine 'xetex)
+(setq TeX-show-compilation t)
+;;;4.6
+(setq TeX-clean-confirm nil)
+
+;;;5.2 Multifile Documents
+(setq Tex-save-query nil)
+;;5.3  Automatic Parsing of TeX Files
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+
+
+
+(provide 'init-auctex)
---
+++ init-reftex.el
@@ -0,0 +1,6 @@
+;;Time-stamp: <2014-07-14 00:46:02 phenix>
+;;default from reftex manual
+
+;;;1.1.3 entering RefTex Mode
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(provide 'init-reftex)
