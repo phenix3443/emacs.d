c898f5ce4dc2a279d3a4fc321b7b96a4ed117496
phenix3443 <phenix3443@gmail.com>
2014-08-04 16:18:06 +0800

    1.fix bug

--- lisps/download_packages.sh
+++ lisps/download_packages.sh
@@ -1,9 +1,3 @@
 !#\bin\bash
-git clone https://github.com/auto-complete/auto-complete.git
-git clone https://github.com/auto-complete/fuzzy-el.git
-git clone https://github.com/auto-complete/popup-el.git
-git clone https://github.com/capitaomorte/yasnippet.git
-git clone https://github.com/m2ym/popwin-el.git
-git clone https://github.com/immerrr/lua-mode.git
 git clone https://github.com/purcell/color-theme-sanityinc-tomorrow.git
 
