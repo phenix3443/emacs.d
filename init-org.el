b79383522b8eac184b5619ecb7267def16993907
phenix3443 <phenix3443@gmail.com>
2014-10-20 20:35:46 +0800

    1.org config

--- init-org.el
+++ init-org.el
@@ -1,5 +1,5 @@
 ; Time-stamp: <2014-08-27 10:11:18 chengxu70>
-; required by text-conf.el, from org manual
+; required by text-conf.el, from org manual(http://orgmode.org/org.html)
 
 (require-package 'org)
 (require-package 'cdlatex)
@@ -9,14 +9,6 @@
 (require 'cdlatex)
 (require 'htmlize)
 
-;; 14.2 Editing source code
-(add-to-list 'org-src-lang-modes
-			 '("xml" . nxml)
-			 '("Shell" . sh))
-(setq org-src-window-setup 'reorganize-frame)
-(setq org-src-preserve-indentation nil)
-(setq org-src-ask-before-returning-to-edit-buffer t)
-(setq org-src-fontify-natively t)
 
 (global-set-key "\C-c'" 'org-src-mode)
 (global-set-key "\C-cl" 'org-store-link)
@@ -37,13 +29,16 @@
 		("elisp" . emacs-lisp)
 		("ocaml" . tuareg)
 		("screen" . shell-script)
+		("shell" . sh)
 		("sql" . sql)
-		("sqlite" . sql)))
-(setq org-src-window-setup reorganize-frame)
+		("sqlite" . sql)
+		("xml" . nxml)))
+(setq org-src-window-setup 'reorganize-frame)
 (setq org-src-preserve-indentation t)
 (setq org-src-ask-before-returning-to-edit-buffer nil)
 
-
+;;15.8 A cleaner outline view
+(setq org-startup-indented t)
 (setq org-startup-folded nil)
 (setq org-list-indent-offset 2)
 (setq org-src-fontify-natively t)
