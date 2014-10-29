e1cf88c9882b3cbfbb86a55eb65400b298b47231
phenix3443 <phenix3443@gmail.com>
2014-10-29 21:48:39 +0800

    1.工程配置。

--- init-projectile.el
+++ init-projectile.el
@@ -1,22 +1,28 @@
 ; Time-stamp: <2014-08-14 22:07:02 chengxu70>
 (require-package 'projectile)
+(require-package 'helm-projectile)
 
 (require 'projectile)
+(projectile-global-mode)
+(require 'helm-projectile)
+(helm-projectile-on)
 
-(add-hook 'lisp-mode-hook 'projectile-mode)
 
-; completion
-(setq projectile-completion-system 'helm)
-
-; caches
+;indexing
+(setq projectile-indexing-method 'alien)
+;caching
 (setq projectile-enable-caching t)
-(setq projectile-remember-window-configs t)
-(setq projectile-file-exists-remote-cache-expire (* 10 60))
-
+;switching projects
+(setq projectile-switch-project-action 'helm-projectile)
+;completion
+(setq projectile-completion-system 'helm)
 ;key binding
 (setq projectile-keymap-prefix (kbd "C-c p"))
 
 ;perspective
+(require-package 'perspective)
+(require 'persp-projectile)
+
 (define-key projectile-mode-map (kbd "s-s") 'projectile-persp-switch-project)
 
 (provide 'init-projectile)
