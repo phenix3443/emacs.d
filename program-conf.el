ea61baea7b0ce2b48ad1d3f4a012e5f463a0db50
phenix3443 <phenix3443@gmail.com>
2014-11-18 02:00:05 +0800

    1.添加luamode

--- program-conf.el
+++ program-conf.el
@@ -33,6 +33,7 @@
 			 (setq c-basic-offset 4)))
 
 ; Lua mode
+(require-package 'lua-mode)
 (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
     (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
     (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
