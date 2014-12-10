4c8ccd01fae9c67c8397fa66d6a3032eba19a981
phenix3443 <phenix3443@gmail.com>
2014-12-10 21:21:23 +0800

    1.helm-gtags 快捷键设置

--- lisps/init-helm-gtags.el
+++ lisps/init-helm-gtags.el
@@ -4,14 +4,36 @@
 
 (require-package 'helm-gtags)
 
-(setq helm-gtags-ignore-case t
-	  helm-gtags-auto-update t
-	  helm-gtags-use-input-at-cursor t
-	  helm-gtags-pulse-at-cursor t
-	  helm-gtags-prefix-key (kbd "C-c g")
-	  helm-gtags-suggested-key-mapping t)
-(require 'helm-gtags)
 (add-hook 'prog-mode-hook 'helm-gtags-mode)
+
+(custom-set-variables
+ '(helm-gtags-ignore-case t)
+ '(helm-gtags-auto-update t)
+ '(helm-gtags-use-input-at-cursor t)
+ '(helm-gtags-pulse-at-cursor t)
+ '(helm-gtags-prefix-key "C-c g")
+ '(helm-gtags-suggested-key-mapping t))
+
+;; key bindings
+(eval-after-load "helm-gtags"
+'(progn
+  (let ((prefix helm-gtags-prefix-key))
+    (define-key helm-gtags-mode-map (concat prefix "h") 'helm-gtags-display-browser)
+    (define-key helm-gtags-mode-map "\C-]" 'helm-gtags-find-tag-from-here)
+    (define-key helm-gtags-mode-map "\C-t" 'helm-gtags-pop-stack)
+    (define-key helm-gtags-mode-map (concat prefix "P") 'helm-gtags-find-files)
+    (define-key helm-gtags-mode-map (concat prefix "f") 'helm-gtags-parse-file)
+    (define-key helm-gtags-mode-map (concat prefix "g") 'helm-gtags-find-pattern)
+    (define-key helm-gtags-mode-map (concat prefix "s") 'helm-gtags-find-symbol)
+    (define-key helm-gtags-mode-map (concat prefix "r") 'helm-gtags-find-rtag)
+    (define-key helm-gtags-mode-map (concat prefix "d") 'helm-gtags-find-tag)
+	(define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
+    ;; common
+    (define-key helm-gtags-mode-map "\e*" 'helm-gtags-pop-stack)
+    (define-key helm-gtags-mode-map "\e." 'helm-gtags-find-tag)
+    (define-key helm-gtags-mode-map "\C-x4." 'helm-gtags-find-tag-other-window))))
+(require 'helm-gtags)
+
 
 (provide 'init-helm-gtags)
 
