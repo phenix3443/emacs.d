f2d4d78f0b67b8be701d2ea964ab7768b0012c28
phenix3443 <phenix3443@gmail.com>
2014-12-11 21:23:10 +0800

    1.添加windows下的环境变量

--- init.el
+++ init.el
@@ -19,7 +19,13 @@
       (is-gnu-kfreebsd-os ())
       (is-darwin-os())
       (is-ms-dos-os ())
-      (is-windows-nt-os())
+      (is-windows-nt-os
+	   '(progn
+		  (setenv "GTAGSCONF" "~/win_apps/glo633wb/share/gtags/gtags.conf")
+		  (setenv "GTAGSLABEL" "pygments")
+		  (setq exec-path '((concat (getenv "emacs_dir") "/bin/")
+						  "C:/Windows/system32/WindowsPowerShell/v1.0"
+						  ))))
       (cygmin-os ()))
 
 ;;use proxy for internet
