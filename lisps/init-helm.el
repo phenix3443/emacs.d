61f8f4fe5dff84610470febde455577a5a6e99f7
phenix3443 <phenix3443@gmail.com>
2014-12-13 00:53:47 +0800

    delete projecitle config from this file

--- lisps/init-helm.el
+++ lisps/init-helm.el
@@ -33,7 +33,6 @@
 	  helm-move-to-line-cycle-in-source t ; move to end or beginning of source when reaching top or bottom of source.
 	  ido-use-virtual-buffers t      ; Needed in helm-buffers-list
 	  helm-buffers-fuzzy-matching t     ; fuzzy matching buffer names when non--nil useful in helm-mini that lists buffers
-	  projectile-enable-caching t
 	  )
 
 ;;Save current position to mark ring when jumping to a different place
