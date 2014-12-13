85119760dc692bdada91bc003d07dc179b80da2a
phenix3443 <phenix3443@gmail.com>
2014-12-13 11:42:05 +0800

    config lua

--- lisps/program-conf.el
+++ lisps/program-conf.el
@@ -59,7 +59,7 @@
 (require-package 'lua-mode)
 (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
 (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
-(add-to-list 'interpreter-mode-alist '("lua52" . lua-mode))
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
 (setq lua-indent-level 4)
 
 ;; python mode
