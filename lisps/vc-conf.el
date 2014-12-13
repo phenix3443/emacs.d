6f227ef0ba42452435fca2645bb1bb2c2bf6b40e
phenix3443 <phenix3443@gmail.com>
2014-12-13 18:54:16 +0800

    add magti info

--- lisps/vc-conf.el
+++ lisps/vc-conf.el
@@ -1,5 +1,9 @@
 ;;;版本控制系统配置
 (require-package 'magit)
 
+(eval-after-load 'info
+  '(progn
+	 (info-initialize)
+	 (add-to-list 'Info-directory-list (file-name-directory (find-library-name "magit")))))
 
 (provide 'vc-conf)
