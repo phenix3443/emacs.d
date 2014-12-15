7f4685d1cf6f18d9c12274b566fe8a0e930c95b6
unknown <chengxu70@CHENGXU70.sygame.com>
2014-12-15 17:50:57 +0800

    configure helm

--- lisps/init-helm.el
+++ lisps/init-helm.el
@@ -3,57 +3,53 @@
 
 (require-package 'helm)
 
-(require 'helm)
-;;; must set before helm-config,  otherwise helm use default
-;;; prefix "C-x c", which is inconvenient because you can
-;;; accidentially pressed "C-x C-c"
-(setq helm-command-prefix-key "C-c h")
-
 (require 'helm-config)
-(require 'helm-eshell)
-(require 'helm-files)
-(require 'helm-grep)
+;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
 
-(setq helm-google-suggest-use-curl-p t
-	  helm-scroll-amount 4 ; scroll 4 lines other window using M-<next>/M-<prior>
-	  helm-quick-update t ; do not display invisible candidates
-	  helm-idle-delay 0.01 ; be idle for this many seconds, before updating in delayed sources.
-	  helm-input-idle-delay 0.01 ; be idle for this many seconds, before updating candidate buffer
-	  helm-ff-search-library-in-sexp t ; search for library in `require' and `declare-function' sexp.
+(setq helm-M-x-fuzzy-match t) ;; optional fuzzy matching for helm-M-x
+;;
+(setq helm-semantic-fuzzy-match t
+	  helm-imenu-fuzzy-match t)
 
-	  helm-split-window-default-side 'other ;; open helm buffer in another window
-	  helm-split-window-in-side-p t ;; open helm buffer inside current window, not occupy whole other window
-	  helm-buffers-favorite-modes (append helm-buffers-favorite-modes
-										  '(picture-mode artist-mode))
-	  helm-candidate-number-limit 200 ; limit the number of displayed canidates
-	  helm-M-x-requires-pattern 0     ; show all candidates when set to 0
-	  helm-boring-file-regexp-list
-	  '("\\.git$" "\\.hg$" "\\.svn$" "\\.CVS$" "\\._darcs$" "\\.la$" "\\.o$" "\\.i$") ; do not show these files in helm buffer
-	  helm-ff-file-name-history-use-recentf t
-	  helm-move-to-line-cycle-in-source t ; move to end or beginning of source when reaching top or bottom of source.
-	  ido-use-virtual-buffers t      ; Needed in helm-buffers-list
-	  helm-buffers-fuzzy-matching t     ; fuzzy matching buffer names when non--nil useful in helm-mini that lists buffers
-	  )
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
 
-;;Save current position to mark ring when jumping to a different place
-(add-hook 'helm-goto-line-before-hook 'helm-save-current-pos-to-mark-ring)
+(when (executable-find "ack-grep")
+  (setq helm-grep-default-command "ack-grep -Hn --no-group --no-color %e %p %f"
+        helm-grep-default-recurse-command "ack-grep -H --no-group --no-color %e %p %f"))
 
-;;key bindings
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t)
+(add-to-list 'helm-sources-using-default-as-input 'helm-source-man-pages)
+(setq helm-locate-fuzzy-match t)
+(setq helm-apropos-function-list t)
+(setq helm-lisp-fuzzy-completion t)
+;; key binding
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+
 (global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "<f8>") 'helm-semantic-or-imenu)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
-
+(global-set-key (kbd "C-h SPC") 'helm-all-mark-rings)
 (global-set-key (kbd "C-c h o") 'helm-occur)
 
-(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to do persistent action
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
 (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+(require 'helm-eshell)
 
-(define-key helm-grep-mode-map (kbd "<return>")  'helm-grep-mode-jump-other-window)
-(define-key helm-grep-mode-map (kbd "n")  'helm-grep-mode-jump-other-window-forward)
-(define-key helm-grep-mode-map (kbd "p")  'helm-grep-mode-jump-other-window-backward)
-
+(add-hook 'eshell-mode-hook
+          #'(lambda ()
+              (define-key eshell-mode-map (kbd "C-c C-l")  'helm-eshell-history)))
+(define-key shell-mode-map (kbd "C-c C-l") 'helm-comint-input-ring)
 (helm-mode 1)
-(provide 'init-helm)
 
+(provide 'init-helm)
