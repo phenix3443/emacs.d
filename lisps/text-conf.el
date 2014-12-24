07daf0c639b0ac2276133c21ac12ae6b992777e3
unknown <chengxu70@CHENGXU70.sygame.com>
2014-12-24 11:08:58 +0800

    make header

--- lisps/text-conf.el
+++ lisps/text-conf.el
@@ -1,4 +1,50 @@
-; Time-stamp: <2014-08-10 21:25:42 phenix>
+;;; text-conf.el --- 
+;; 
+;; Filename: text-conf.el
+;; Description: 
+;; Author: phenix34433<phenix3443@gmail.com>
+;; Maintainer: 
+;; Created: 周三 十二月 24 11:07:52 2014 (+0800)
+;; Version: 
+;; Package-Requires: ()
+;; Last-Updated: 周三 十二月 24 11:08:46 2014 (+0800)
+;;           By: chengxu70
+;;     Update #: 1
+;; URL: 
+;; Doc URL: 
+;; Keywords: 
+;; Compatibility: 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Commentary: 
+;; 
+;; 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Change Log:
+;; 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Code:
+
 ;; 25.2 sentences
 (setq sentence-end "\\([。！？]\\|……\\|[.?!][]\"')}]*\\($\\|[ \t]\\)\\)[ \t\n]*")
 (setq sentence-end-double-space nil)
@@ -11,5 +57,7 @@
 (require 'init-org)
 (require 'init-org2blog)
 
-
 (provide 'text-conf)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; text-conf.el ends here
