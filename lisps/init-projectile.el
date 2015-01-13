c3b7011ead89ff71df7ce6bb41e3b55189555fab
phenix3443 <phenix3443@gmail.com>
2015-01-13 20:37:17 +0800

    添加make-header

--- lisps/init-projectile.el
+++ lisps/init-projectile.el
@@ -1,13 +1,60 @@
-;;; Time-stamp: <2014-08-14 22:07:02 chengxu70>
+;;; init-projectile.el --- 
+;; 
+;; Filename: init-projectile.el
+;; Description: 
+;; Author: phenix3443(phenix3443@gmail.com)
+;; Maintainer: 
+;; Created: 周二 一月 13 19:42:55 2015 (+0800)
+;; Version: 
+;; Package-Requires: ()
+;; Last-Updated: 周二 一月 13 19:55:18 2015 (+0800)
+;;           By: chengxu70
+;;     Update #: 2
+;; URL: 
+;; Doc URL: 
+;; Keywords: 
+;; Compatibility: 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Commentary: 
+;; 
+;; 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Change Log:
+;; 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Code:
+
 (require-package 'projectile)
 
 (require 'projectile)
 (projectile-global-mode)
-
+;; Helm Integration
 (require-package 'helm-projectile)
 (require 'helm-projectile)
 (helm-projectile-on)
 
+(require-package 'helm-ag)
 ;; indexing
 ;;(setq projectile-indexing-method 'alien)
 ;; caching
@@ -25,3 +72,6 @@
 (require-package 'persp-projectile)
 
 (provide 'init-projectile)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; init-projectile.el ends here
