f394ee4cc29b70b3a18d53b36e4081155481f084
phenix3443 <phenix3443@gmail.com>
2015-01-21 23:50:21 +0800

    config chinese-pyim

---
+++ lisps/international-conf.el
@@ -0,0 +1,69 @@
+;;; international-conf.el --- 
+;; 
+;; Filename: international-conf.el
+;; Description: 
+;; Author: phenix <phenix3443@gmail.com>
+;; Maintainer: 
+;; Created: 三  1月 21 22:42:59 2015 (+0800)
+;; Version: 
+;; Package-Requires: ()
+;; Last-Updated: 三  1月 21 23:50:05 2015 (+0800)
+;;           By: phenix
+;;     Update #: 9
+;; URL: 
+;; Doc URL: 
+;; Keywords: 
+;; Compatibility: 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Commentary: 
+;; 
+;; 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Change Log:
+;; 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Code:
+;; required by init.el
+
+;; 22.3 Input Methods
+
+;; (defun my-chinese-setup ()
+;;   "Set up my private Chinese environment."
+;;   (if (equal current-language-environment "Chinese-GBK")
+;; 	  (progn
+;; 		(require-package 'chinese-pyim)
+;; 		(require 'chinese-pyim)
+;; 		(setq pyim-dicts '((:name "BgiDict" :file "~/.emacs.d/pyim/pyim-bigdict.txt" :coding utf-8-unix)))
+;; 		(setq default-input-method "chinese-pyim"))))
+
+;; (add-hook 'set-language-environment-hook 'my-chinese-setup)
+
+(require-package 'chinese-pyim)
+(require 'chinese-pyim)
+(setq pyim-dicts '((:name "BgiDict" :file "~/.emacs.d/pyim/pyim-bigdict.txt" :coding utf-8-unix)))
+(setq default-input-method "chinese-pyim")
+
+(provide 'international-conf)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; international-conf.el ends here
