fc686afc790f96cfec50bddf2a90bac861ae403c
phenix3443 <phenix3443@gmail.com>
2015-03-07 16:30:46 +0800

    不再使用reftex

--- lisps/init-reftex.el
+++
@@ -1,9 +0,0 @@
-; Time-stamp: <2014-08-27 10:13:40 chengxu70>
-(require-package 'reftex)
-; required by text-conf.el
-; default from reftex manual
-
-;;;1.1.3 entering RefTex Mode
-(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-
-(provide 'init-reftex)
