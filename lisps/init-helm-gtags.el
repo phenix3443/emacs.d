62be9235fbece55fe54e689c612f964f0dbe290d
phenix3443 <phenix3443@gmail.com>
2015-05-06 00:48:08 +0800

    add file header

--- lisps/init-helm-gtags.el
+++ lisps/init-helm-gtags.el
@@ -1,4 +1,50 @@
-;;; Time-stamp: <2014-08-27 09:54:15 chengxu70>
+;;; init-helm-gtags.el ---
+;;
+;; Filename: init-helm-gtags.el
+;; Description:
+;; Author: phenix3443 <phenix3443@gmail.com>
+;; Maintainer:
+;; Created: 三  5月  6 00:38:51 2015 (+0800)
+;; Version:
+;; Package-Requires: ()
+;; Last-Updated:
+;;           By:
+;;     Update #: 3
+;; URL:
+;; Doc URL:
+;; Keywords:
+;; Compatibility:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change Log:
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
 ;;; https://https://github.com/syohex/emacs-helm-gtags
 ;;; required by program-conf.el
 
@@ -36,7 +82,7 @@
 	   (define-key helm-gtags-mode-map "\C-x4." 'helm-gtags-find-tag-other-window))))
 (require 'helm-gtags)
 
-
 (provide 'init-helm-gtags)
 
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; init-helm-gtags.el ends here
