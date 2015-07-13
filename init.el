9fec7b3586d043a8ddb0c364a9fda6b162a4a9b9
unknown <chengxu70@CHENGXU70.sygame.com>
2015-07-13 16:13:06 +0800

    淇敼gtags榛樿閰嶇疆

--- init.el
+++ init.el
@@ -1,48 +1,48 @@
 ;;; init.el --- emacs init file
-;; 
+;;
 ;; Filename: init.el
-;; Description: 
+;; Description:
 ;; Author: phenix3443<phenix3443@gmail.com>
-;; Maintainer: 
+;; Maintainer:
 ;; Created: 周四 十二月 18 16:07:09 2014 (+0800)
 ;; Version: 1.0
 ;; Package-Requires: ()
-;; Last-Updated: 周六 二月  7 12:51:56 2015 (+0800)
+;; Last-Updated: 周一 七月 13 16:07:10 2015 (+0800)
 ;;           By: chengxu70
 ;;     Update #:
-;; URL: 
-;; Doc URL: 
+;; URL:
+;; Doc URL:
 ;; Keywords: init
-;; Compatibility: 
-;; 
+;; Compatibility:
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
-;;; Commentary: 
-;; 
-;; 
-;; 
+;;
+;;; Commentary:
+;;
+;;
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Change Log:
-;; 
-;; 
+;;
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or (at
 ;; your option) any later version.
-;; 
+;;
 ;; This program is distributed in the hope that it will be useful, but
 ;; WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;; General Public License for more details.
-;; 
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
-;; 
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Code:
 ;; (setq debug-on-error t)
 
@@ -54,10 +54,6 @@
 (defconst is-windows-nt-os (equal system-type 'windows-nt) "native win32 application")
 (defconst is-cygwin-os (equal system-type 'cygwin) "cygwin")
 
-(when (equal system-type 'windows-nt)
-  ;; 不要在emacs里面改动 PATH 和 exec-path
-  (setenv "GTAGSCONF" (concat (getenv "HOME") "\\.emacs.d\\win_apps\\glo634wb\\share\\gtags\\gtags.conf"))
-  (setenv "GTAGSLABEL" "pygments"))
 (defconst tmp-dir "~/.emacs.d/tmp-dir/")
 
 ;; use proxy for internet
