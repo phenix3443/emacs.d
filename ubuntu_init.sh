2bdee953f39c65a15f77032e79fe2cc09ae54136
unknown <chengxu70@CHENGXU70.sygame.com>
2015-07-14 16:30:24 +0800

    add global enviroment var

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -3,3 +3,8 @@ echo 执行本脚本说明你现在准备使用phenix3443的emacs配置文件。
 echo ubuntu_init.sh主要用来安装一些独立的第三方软件，设置必要的环境变量。
 echo 请在克隆.emacs.d文件夹后，启动emacs之前执行该脚本。
 echo 如有问题请联系phenix3443@gmail.com
+
+
+#设置global
+echo GTAGSLABEL=pygments >> ~/.bashrc
+echo GTAGSCONF="" >> ~/.bashrc
