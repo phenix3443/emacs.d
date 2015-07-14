b0374b455aa83f2f22aa0ac1dd2cf7b6ce9505df
unknown <chengxu70@CHENGXU70.sygame.com>
2015-07-14 12:34:20 +0800

    some test on magit

--- lisps/vc-conf.el
+++ lisps/vc-conf.el
@@ -1,9 +1,59 @@
-;;;版本控制系统配置
+;;; vc-conf.el ---
+;;
+;; Filename: vc-conf.el
+;; Description:
+;; Author: phenix3443<phenix3443@gmail.com>
+;; Maintainer: phenix3443<phenix3443@gmail.com>
+;; Created: 周二 七月 14 12:32:43 2015 (+0800)
+;; Version:
+;; Package-Requires: ()
+;; Last-Updated:
+;;           By:
+;;     Update #: 1
+;; URL:
+;; Doc URL:
+;; Keywords:
+;; Compatibility:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change Log:
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+
+
 (require-package 'magit)
 
-(eval-after-load 'info
-  '(progn
-	 (info-initialize)
-	 (add-to-list 'Info-directory-list (file-name-directory (find-library-name "magit")))))
+(with-eval-after-load 'info
+  (info-initialize)
+  (add-to-list 'Info-directory-list (file-name-directory (find-library-name "magit"))))
 
 (provide 'vc-conf)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; vc-conf.el ends here
