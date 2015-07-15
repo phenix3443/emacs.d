3ad6dbaf37a4c75a63121e9b4f8e7d1f938fcfcd
unknown <chengxu70@CHENGXU70.sygame.com>
2015-07-15 18:16:47 +0800

    add window management

--- lisps/window-conf.el
+++ lisps/window-conf.el
@@ -1,7 +1,61 @@
-;;;widnow configure
+;;; window-conf.el ---
+;;
+;; Filename: window-conf.el
+;; Description: emacs windows manager
+;; Author: phenix <phenix3443@gmail.com>
+;; Maintainer: phenix <phenix3443@gmail.com>
+;; Created: 周三 七月 15 17:38:39 2015 (+0800)
+;; Version:
+;; Package-Requires: ()
+;; Last-Updated:
+;;           By:
+;;     Update #: 7
+;; URL:
+;; Doc URL:
+;; Keywords:
+;; Compatibility:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change Log:
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+
+
 
 ;; 20.3 Using Other Windows
-(when (fboundp 'windmove-default-keybindings)
-  (windmove-default-keybindings))
+(require-package 'window-numbering)
+(window-numbering-mode 1)
+
+;; 20.7 Convenience Features for Window Handling
+(winner-mode 1)
 
 (provide 'window-conf)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; window-conf.el ends here
