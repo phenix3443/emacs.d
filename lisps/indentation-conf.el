f3b4fbbac39f4e4795d84ac8d8a3b98ce3e28360
phenix3443 <phenix3443@gmail.com>
2015-07-16 23:00:42 +0800

    start indent-guide

--- lisps/indentation-conf.el
+++ lisps/indentation-conf.el
@@ -1,48 +1,48 @@
-;;; indentation-conf.el --- 
-;; 
+;;; indentation-conf.el ---
+;;
 ;; Filename: indentation-conf.el
-;; Description: 
+;; Description:
 ;; Author: phenix3443(phenix3443@gmail.com)
-;; Maintainer: 
+;; Maintainer:
 ;; Created: 周六 一月 17 10:10:24 2015 (+0800)
-;; Version: 
+;; Version:
 ;; Package-Requires: ()
-;; Last-Updated: 周六 一月 17 16:11:58 2015 (+0800)
-;;           By: chengxu70
-;;     Update #: 6
-;; URL: 
-;; Doc URL: 
-;; Keywords: 
-;; Compatibility: 
-;; 
+;; Last-Updated: 周四 7月 16 23:00:42 2015 (+0800)
+;;           By: phenix
+;;     Update #: 8
+;; URL:
+;; Doc URL:
+;; Keywords:
+;; Compatibility:
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
-;;; Commentary: 
-;; 
-;; 
-;; 
+;;
+;;; Commentary:
+;;
+;;
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Change Log:
-;; 
-;; 
+;;
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or (at
 ;; your option) any later version.
-;; 
+;;
 ;; This program is distributed in the hope that it will be useful, but
 ;; WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;; General Public License for more details.
-;; 
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
-;; 
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Code:
 
 ;;24.3 Tabs vs Spaces
@@ -55,10 +55,10 @@
 (provide 'indentation-conf)
 
 ;;; show vertical lines to guide indentation
-;; (require-package 'indent-guide)
-;; (indent-guide-global-mode)
-;; (setq indent-guide-delay 0.1)
-;; (setq indent-guide-recursive t)
+(require-package 'indent-guide)
+(indent-guide-global-mode)
+(setq indent-guide-delay 0.1)
+(setq indent-guide-recursive t)
 
 ;;;
 ;; (require-package 'highlight-indentation)
