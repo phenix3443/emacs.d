f35d83e2c5a4d62c383b7885f8e6935241274cb6
unknown <chengxu70@CHENGXU70.sygame.com>
2015-07-16 16:56:31 +0800

    璋冩暣寮曠敤鐨勬枃妗ｉ『搴?

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
 ;; Created: 周四 十二月 18 16:07:09 2014 (+0800)
 ;; Version: 1.0
 ;; Package-Requires: ()
-;; Last-Updated: 周四 七月 16 13:26:11 2015 (+0800)
+;; Last-Updated: 周四 七月 16 16:54:52 2015 (+0800)
 ;;           By: chengxu70
 ;;     Update #:
 ;; URL:
@@ -66,13 +66,13 @@
 		;; ("https" . "127.0.0.1:10800")))
 
 ;;加载配置文件，按照 https://www.gnu.org/software/emacs/manual/ 出现顺序
-;; GNU Emacs manual.
+
 (defun load-package-init-files()
   "load most packages init files"
   (let ((default-directory "~/.emacs.d/lisps"))
 	(normal-top-level-add-to-load-path '("."))
 	(normal-top-level-add-subdirs-to-load-path))
-
+  ;; GNU Emacs manual.
   (require 'misc-conf)
   (require 'packages-conf)				;47
   (require 'minibuffer-conf)			;8
@@ -89,73 +89,73 @@
   (require 'compile-test-conf)			;27
   (require 'vc-conf)					;28
   (require 'customization-conf)			;48
-  (require 'init-header2))
+  (require 'init-header2)
 
-;; GNU Emacs Lisp reference manual.
-;; An Introduction to Programming in Emacs Lisp.
-;; GNU Emacs FAQ.
-;; GNU Emacs on MS Windows FAQ.
-;; GNU Emacs reference cards.
-;; Ada-mode(Emacs mode for editing Ada code.)
-;; Auth-source	Emacs library for storing and sharing secret data.
-;; Autotype	Features for frequently-entered text.
-;; Calc	Calc is an advanced calculator and mathematical tool.
-;; CC mode	Mode for editing C, C++, Objective C, Java, Pike, and IDL code.
-(setq c-default-style '((java-mode . "java")
-						(awk-mode . "awk")
-						(other . "linux")))
+  ;; GNU Emacs Lisp reference manual.
+  ;; An Introduction to Programming in Emacs Lisp.
+  ;; GNU Emacs FAQ.
+  ;; GNU Emacs on MS Windows FAQ.
+  ;; GNU Emacs reference cards.
+  ;; Ada-mode(Emacs mode for editing Ada code.)
+  ;; Auth-source	Emacs library for storing and sharing secret data.
+  ;; Autotype	Features for frequently-entered text.
+  ;; Calc	Calc is an advanced calculator and mathematical tool.
+  ;; CC mode	Mode for editing C, C++, Objective C, Java, Pike, and IDL code.
+  (setq c-default-style '((java-mode . "java")
+						  (awk-mode . "awk")
+						  (other . "linux")))
 
-;; CL	GNU Emacs Common Lisp support.
-;; D-Bus	Emacs API for using the D-Bus message bus system.
-;; Dired-X	Extra Dired features.
-;; Ebrowse	C++ class browser.
-;; EDE	Package to simplify building and debugging programs.
-;; EDT	EDT Emulator.
-;; Ediff	Visual interface for comparing and merging programs.
-;; EIEIO	Common Lisp Object System library for Emacs.
-;; Emacs GnuTLS	Emacs interface to the GnuTLS library.
-;; Emacs MIME	Emacs library for composing and showing MIME messages.
-;; EasyPG Assistant	Emacs user interface to the GNU Privacy Guard.
-;; ERC	Modular and extensible IRC client for Emacs.
-;; ERT	The Emacs Lisp Regression Testing tool.
-;; Eshell	Command shell implemented in Emacs Lisp.
-;; EUDC	The Emacs Unified Directory Client.
-;; EWW	A web browser.
-;; Forms	Mode for manipulating text forms.
-;; Flymake	On-the-fly programming language syntax checker.
-;; Gnus	Mail and news reader.
-;; Htmlfontify	Export buffers to HTML.
-;; IDLWAVE	Major mode and shell for IDL and WAVE/CL files.
-;; IDO	A package for interactively doing things.
-;; Info	The Info documentation browser.
-;; Mairix	Emacs interface to the Mairix email indexing tool.
-;; Message	Mail and news composition mode for Gnus.
-;; MH-E	Interface to the MH mail system.
-;; Newsticker	A newsticker for Emacs.
-;; nXML	XML editing mode with RELAX NG support.
-;; Octave mode	Major mode for GNU Octave code.
-;; Org mode	Outline-based note manager and organizer.
-;; PCL-CVS	Front-end to CVS.
-;; PGG	Obsolete interface library for secure communication; see EasyPG.
-;; RCIRC	IRC client for Emacs.
-;; RefTeX	Manage labels, references, citations and index entries.
-;; Remember	Tool for managing todo lists, schedules, and other data.
-;; SASL	Emacs interface to the SASL (Simple Authentication and Security Layer) framework.
-;; Semantic	Source code parsing utilities for Emacs.
-;; Semantic parser (bovine)	The original Semantic parser.
-;; Semantic Recoder	Converts information from Semantic back into code.
-;; SES	The Simple Emacs Spreadsheet.
-;; Sieve	Mode for the Sieve mail filtering language.
-;; SMTP	Emacs library for sending mail via SMTP.
-;; Speedbar	File and tag summarizing utility.
-;; Supercite	Package for citing and attributing messages.
-;; Todo mode	Keeping track of things to do.
-;; Tramp	Transparent remote file access system.
-;; URL	Library for manipulating and retrieving URLs and URIs.
-;; VIP	An older VI emulator.
-;; Viper	VI emulator.
-;; Widget	Library for graphical widgets.
-;; Wisent	An Emacs implementation of the GNU Compiler Compiler Bison.
-;; Woman	Browsing UN*X pages without man.
-
+  ;; CL	GNU Emacs Common Lisp support.
+  ;; D-Bus	Emacs API for using the D-Bus message bus system.
+  ;; Dired-X	Extra Dired features.
+  ;; Ebrowse	C++ class browser.
+  ;; EDE	Package to simplify building and debugging programs.
+  ;; EDT	EDT Emulator.
+  ;; Ediff	Visual interface for comparing and merging programs.
+  ;; EIEIO	Common Lisp Object System library for Emacs.
+  ;; Emacs GnuTLS	Emacs interface to the GnuTLS library.
+  ;; Emacs MIME	Emacs library for composing and showing MIME messages.
+  ;; EasyPG Assistant	Emacs user interface to the GNU Privacy Guard.
+  ;; ERC	Modular and extensible IRC client for Emacs.
+  ;; ERT	The Emacs Lisp Regression Testing tool.
+  ;; Eshell	Command shell implemented in Emacs Lisp.
+  ;; EUDC	The Emacs Unified Directory Client.
+  ;; EWW	A web browser.
+  ;; Forms	Mode for manipulating text forms.
+  ;; Flymake	On-the-fly programming language syntax checker.
+  ;; Gnus	Mail and news reader.
+  ;; Htmlfontify	Export buffers to HTML.
+  ;; IDLWAVE	Major mode and shell for IDL and WAVE/CL files.
+  ;; IDO	A package for interactively doing things.
+  ;; Info	The Info documentation browser.
+  ;; Mairix	Emacs interface to the Mairix email indexing tool.
+  ;; Message	Mail and news composition mode for Gnus.
+  ;; MH-E	Interface to the MH mail system.
+  ;; Newsticker	A newsticker for Emacs.
+  ;; nXML	XML editing mode with RELAX NG support.
+  ;; Octave mode	Major mode for GNU Octave code.
+  ;; Org mode	Outline-based note manager and organizer.
+  ;; PCL-CVS	Front-end to CVS.
+  ;; PGG	Obsolete interface library for secure communication; see EasyPG.
+  ;; RCIRC	IRC client for Emacs.
+  ;; RefTeX	Manage labels, references, citations and index entries.
+  ;; Remember	Tool for managing todo lists, schedules, and other data.
+  ;; SASL	Emacs interface to the SASL (Simple Authentication and Security Layer) framework.
+  ;; Semantic	Source code parsing utilities for Emacs.
+  ;; Semantic parser (bovine)	The original Semantic parser.
+  ;; Semantic Recoder	Converts information from Semantic back into code.
+  ;; SES	The Simple Emacs Spreadsheet.
+  ;; Sieve	Mode for the Sieve mail filtering language.
+  ;; SMTP	Emacs library for sending mail via SMTP.
+  ;; Speedbar	File and tag summarizing utility.
+  ;; Supercite	Package for citing and attributing messages.
+  ;; Todo mode	Keeping track of things to do.
+  ;; Tramp	Transparent remote file access system.
+  ;; URL	Library for manipulating and retrieving URLs and URIs.
+  ;; VIP	An older VI emulator.
+  ;; Viper	VI emulator.
+  ;; Widget	Library for graphical widgets.
+  ;; Wisent	An Emacs implementation of the GNU Compiler Compiler Bison.
+  ;; Woman	Browsing UN*X pages without man.
+  )
 (add-hook 'after-init-hook 'load-package-init-files)
