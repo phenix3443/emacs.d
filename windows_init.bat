6dce88ff870e5601bbcca27777ff3c37ba238aaa
phenix3443 <phenix3443@gmail.com>
2015-07-24 18:21:52 +0800

    修改windows 初始化脚本

--- windows_init.bat
+++ windows_init.bat
@@ -3,8 +3,8 @@ echo windows_init.bat主要用来安装一些独立的第三方软件，设置必要的环境变量。
 echo 请在克隆.emacs.d文件夹后，启动emacs之前执行该脚本。
 echo 如有问题请联系phenix3443@gmail.com
 
-#请自行修改该行配置，这是我的Ubuntu虚拟机，该配置针对tramp进行配置。
-#echo 192.168.40.134 vm_ubuntu  >>  %windir%\system32\drivers\etc\hosts
+echo 修改环境变量
+
 
 echo 没有找到解压winRAR，需要自行解压%APPDATA%\.emacs.d\win_apps\glo65wb.zip到文件所在目录。
 echo 需要自行安装%APPDATA%\.emacs.d\win_apps\gpg4win-2.2.5.exe
