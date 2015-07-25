044579da81a36f6dde4145aca1e3159c8a4bdb15
phenix3443 <phenix3443@gmail.com>
2015-07-26 01:37:53 +0800

    添加解压global的代码和unzip程序

--- windows_init.bat
+++ windows_init.bat
@@ -1,11 +1,11 @@
-echo 执行本脚本说明你现在准备使用phenix3443的emacs配置文件。
-echo windows_init.bat主要用来安装一些独立的第三方软件，设置必要的环境变量。
-echo 请在克隆.emacs.d文件夹后，启动emacs之前执行该脚本。
-echo 如有问题请联系phenix3443@gmail.com
+rem添加第三方工具路径到path环境变量
+wmic ENVIRONMENT where "name='path' and username='<system>'" set VariableValue="%path%;%APPDATA%\.emacs.d\win_apps\;%APPDATA%\win_apps\glo65wb\bin"
+rem 添加gnu global使用的环境变量
+wmic ENVIRONMENT create name="GTAGSCONF",username="<system>",VariableValue="%APPDATA%\.emacs.d\win_apps\glo65wb\share\gtags\gtags.conf"
+wmic ENVIRONMENT create name="GTAGSLABEL",username="<system>",VariableValue="pygment"
+rem 解压GNU Global
+unzip -o glo65wb.zip
 
-echo 修改环境变量
 
-
-echo 没有找到解压winRAR，需要自行解压%APPDATA%\.emacs.d\win_apps\glo65wb.zip到文件所在目录。
-echo 需要自行安装%APPDATA%\.emacs.d\win_apps\gpg4win-2.2.5.exe
-echo 现在可以启动Emacs，等待其通过ELPA安装需要的package。
+echo Done successfully!
+echo Restart emacs and wait a moment for installing dependency packages from ELPA;
