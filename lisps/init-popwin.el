5cbc053ca6e07ea0f8f293d0170ffe35bad1a85d
phenix3443 <phenix3443@gmail.com>
2015-07-31 18:20:19 +0800

    guide-key显示在下面

--- lisps/init-popwin.el
+++ lisps/init-popwin.el
@@ -1,4 +1,49 @@
-; Time-stamp: <2014-08-11 14:47:21 chengxu70>
+;;; init-popwin.el ---
+;;
+;; Filename: init-popwin.el
+;; Description:
+;; Author: phenix3443 <phenix3443@gmail.com>
+;; Maintainer: phenix3443
+;; Created: 周五 7月 31 17:26:38 2015 (+0800)
+;; Version:
+;; Package-Requires: ()
+;; Last-Updated:
+;;           By:
+;;     Update #: 2
+;; URL:
+;; Doc URL:
+;; Keywords:
+;; Compatibility:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change Log:
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
 (require-package 'popwin)
 
 (require 'popwin)
@@ -20,3 +65,6 @@
 (add-to-list 'popwin:special-display-config '(" *auto-async-byte-compile*" :noselect))
 
 (provide 'init-popwin)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; init-popwin.el ends here
