0650fad6c35132aaaabecf1e98744d237f7e88ad
phenix3443 <phenix3443@gmail.com>
2015-09-23 10:53:22 +0800

    some change

--- lisps/init-cc-mode.el
+++ lisps/init-cc-mode.el
@@ -1,48 +1,48 @@
-;;; init-cc-mode.el --- 
-;; 
+;;; init-cc-mode.el ---
+;;
 ;; Filename: init-cc-mode.el
-;; Description: 
+;; Description:
 ;; Author: phenix <phenix3443@gmail.com>
-;; Maintainer: 
+;; Maintainer:
 ;; Created: 四  2月  5 02:18:07 2015 (+0800)
-;; Version: 
+;; Version:
 ;; Package-Requires: ()
-;; Last-Updated: 周四 二月  5 10:47:02 2015 (+0800)
-;;           By: chengxu70
-;;     Update #: 4
-;; URL: 
-;; Doc URL: 
-;; Keywords: 
-;; Compatibility: 
-;; 
+;; Last-Updated: 五  9月  4 19:24:45 2015 (+0800)
+;;           By: ubuntu
+;;     Update #: 5
+;; URL:
+;; Doc URL:
+;; Keywords:
+;; Compatibility:
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
-;;; Commentary: 
-;; 
-;; 
-;; 
+;;
+;;; Commentary:
+;;
+;;
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Change Log:
-;; 
-;; 
+;;
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or (at
 ;; your option) any later version.
-;; 
+;;
 ;; This program is distributed in the hope that it will be useful, but
 ;; WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;; General Public License for more details.
-;; 
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
-;; 
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Code:
 
 (setq c-default-style '((java-mode . "java")
