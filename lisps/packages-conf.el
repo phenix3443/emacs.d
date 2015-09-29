74ae6329e7ec44645999489db4a3a1597f2341d4
phenix3443 <phenix3443@gmail.com>
2015-09-29 10:36:13 +0800

    add magit

--- lisps/packages-conf.el
+++ lisps/packages-conf.el
@@ -75,8 +75,8 @@
   (require 'init-cedet)
   (require 'init-chinese-pyim)
   ;; (require 'init-company)
-  (require 'init-csharp-mode)
-  (require 'init-ecb)
+  ;;(require 'init-csharp-mode)
+  ;;(require 'init-ecb)
   (require 'init-flycheck)
   ;; (require 'init-ggtags)
   (require 'init-guide-key)
@@ -94,7 +94,7 @@
   (require 'init-paredit)
   (require 'init-popwin)  ;; auto-complete configure
   (require 'init-projectile);;project support
-  (require 'init-python-mode)
+  ;;(require 'init-python-mode)
   (require 'init-smartparens)
   (require 'init-themes)
   (require 'init-window-numbering)
