3b2dc62bc5f8d75c3835e9b4ad1df5816b0cb16b
phenix3443 <phenix3443@gmail.com>
2015-10-01 17:38:31 +0800

    瀹屾垚clone涔嬪悗windows鐜鐨勫垵濮嬪寲璁剧疆

--- windows_init.bat
+++ windows_init.bat
@@ -1,22 +1,48 @@
+@echo off
+echo 默认将配置文件放在%APPDATA%\.emacs.d中
+rem git clone git@github.com:phenix3443/emacs.d.git %APPDATA%\.emacs.d
+cd %~dp0\
 
-rem 添加python执行路径到环境变量path
-set python_install_path=C:\Python27\
-wmic ENVIRONMENT where "name='path' and username='<system>'" set VariableValue="%path%;%python_install_path%;%python_install_path%\Scripts\"
+rem emacs.d安装路径
+set emacs_d_path=%APPDATA%\.emacs.d
 
-rem install pygments
-python %emacs_d_path%\win_apps\python get-pip.py
+echo 删除已有的HOME环境变量
+wmic ENVIRONMENT where "name='HOME'" delete
+echo 添加HOME环境变量
+wmic ENVIRONMENT create name="HOME",username="<system>",VariableValue=%APPDATA%
+
+echo Path环境变量中添加Python27的可执行文件路径，包括通过pip安装的
+rem python27安装路径
+set python_install_path=C:\Python27
+set path=%path%;%python_install_path%;%python_install_path%\Scripts\
+wmic ENVIRONMENT where "name='path' and username='<system>'" set VariableValue="%path%"
+echo 安装Pygments
 pip install Pygments
 
-rem 添加第三方工具路径到path环境变量
-wmic ENVIRONMENT where "name='path' and username='<system>'" set VariableValue="%path%;%APPDATA%\.emacs.d\win_apps;%path%;%APPDATA%\.emacs.d\win_apps\glo65wb\bin\"
+echo 添加第三方工具（es.exe,lua.exe等）到path环境变量
+set path=%path%;%emacs_d_path%\win_apps\
+wmic ENVIRONMENT where "name='path' and username='<system>'" set VariableValue="%path%"
 
-rem 添加gnu global使用的环境变量
-wmic ENVIRONMENT create name="GTAGSCONF",username="<system>",VariableValue="%APPDATA%\.emacs.d\win_apps\glo65wb\share\gtags\gtags.conf"
+echo 解压GNU Global
+rem %~dp0\win_apps\unzip -d %~dp0\win_apps\ -o %~dp0\win_apps\glo651wb.zip
+win_apps\unzip -d win_apps\ -o win_apps\glo651wb.zip
+
+echo 添加GNU Global到path环境变量
+set path=%path%;%emacs_d_path%\win_apps\glo651wb\bin\
+wmic ENVIRONMENT where "name='path' and username='<system>'" set VariableValue="%path%"
+
+echo 创建环境变量GTAGSCONF
+rem 删除已有的GTAGSCONF环境变量
+wmic ENVIRONMENT where "name='GTAGSCONF'" delete
+wmic ENVIRONMENT create name="GTAGSCONF",username="<system>",VariableValue="%emacs_d_path%\win_apps\glo651wb\share\gtags\gtags.conf"
+echo 创建环境变量GTAGSLABEL
+rem 删除已有的GTAGSLABEL环境变量
+wmic ENVIRONMENT where "name='GTAGSLABEL'" delete
 wmic ENVIRONMENT create name="GTAGSLABEL",username="<system>",VariableValue="pygments"
 
-rem 解压GNU Global
-unzip -o glo65wb.zip
-
+gtags --debug
 
 echo Done successfully!
-echo Restart emacs and wait a moment for installing dependency packages from ELPA;
+echo 现在启动emacs之后先从elpa下载相应的扩展插件
+
+pause
