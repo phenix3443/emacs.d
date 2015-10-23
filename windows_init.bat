9e6e108617adcb8771a7f270e8d94ddf2cfbd06f
phenix3443 <phenix3443@gmail.com>
2015-10-23 22:38:14 +0800

    delete gtagsconf env

--- windows_init.bat
+++ windows_init.bat
@@ -3,10 +3,8 @@ echo 默认将配置文件放在%APPDATA%\.emacs.d中
 rem git clone git@github.com:phenix3443/emacs.d.git %APPDATA%\.emacs.d
 cd %~dp0\
 
-rem emacs.d安装路径
-set emacs_d_path=%APPDATA%\.emacs.d
-
-echo Path环境变量中添加Python27的可执行文件路径，包括通过pip安装的
+echo check python2.7
+rem todo: check python2.7, if not installed, install it
 rem python27安装路径
 set python_install_path=C:\Python27
 set path=%path%;%python_install_path%;%python_install_path%\Scripts\
@@ -14,26 +12,14 @@ wmic ENVIRONMENT where "name='path' and username='<system>'" set VariableValue="
 echo 安装Pygments
 pip install Pygments
 
-echo 添加第三方工具（es.exe,lua.exe等）到path环境变量
-set path=%path%;%emacs_d_path%\win_apps\
+set path=%path%;%APPDATA%\.emacs.d\win_apps\
 wmic ENVIRONMENT where "name='path' and username='<system>'" set VariableValue="%path%"
 
-echo 解压GNU Global
-rem %~dp0\win_apps\unzip -d %~dp0\win_apps\ -o %~dp0\win_apps\glo651wb.zip
+echo install GNU Global
 win_apps\unzip -d win_apps\ -o win_apps\glo651wb.zip
-
-echo 添加GNU Global到path环境变量
-set path=%path%;%emacs_d_path%\win_apps\glo651wb\bin\
+set path=%path%;%APPDATA%\.emacs.d\win_apps\glo651wb\bin\
 wmic ENVIRONMENT where "name='path' and username='<system>'" set VariableValue="%path%"
-
-echo 创建环境变量GTAGSCONF
-rem 删除已有的GTAGSCONF环境变量
-wmic ENVIRONMENT where "name='GTAGSCONF'" delete
-wmic ENVIRONMENT create name="GTAGSCONF",username="<system>",VariableValue="%emacs_d_path%\win_apps\glo651wb\share\gtags\gtags.conf"
-echo 创建环境变量GTAGSLABEL
-rem 删除已有的GTAGSLABEL环境变量
-wmic ENVIRONMENT where "name='GTAGSLABEL'" delete
-wmic ENVIRONMENT create name="GTAGSLABEL",username="<system>",VariableValue="pygments"
+cp \win_apps\.globalrc %APPDATA%
 
 gtags --debug
 
