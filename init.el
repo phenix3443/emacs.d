0ff8dae13f6653e098ed1aa060d2a3cb2754fee1
phenix3443 <phenix3443@gmail.com>
2015-10-24 17:50:06 +0800

    add session

--- init.el
+++ init.el
@@ -7,8 +7,8 @@
 ;; Created: 周四 十二月 18 16:07:09 2014 (+0800)
 ;; Version: 1.0
 ;; Package-Requires: ()
-;; Last-Updated: 日  9月 20 23:38:23 2015 (+0800)
-;;           By: ubuntu
+;; Last-Updated: 周六 10月 24 17:50:06 2015 (+0800)
+;;           By: phenix
 ;;     Update #:
 ;; URL:
 ;; Doc URL:
@@ -90,6 +90,9 @@
 ;; (require 'maintain-program-project)
 ;; 43 Saving Emacs Sessions
 ;; (desktop-save-mode 1)
+(require-package 'session)
+(require 'session)
+(add-hook 'after-init-hook 'session-initialize)
 
 ;; 47 Emacs Lisp Packages
 (require 'packages-conf)
