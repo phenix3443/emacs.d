1f98b82b8e50a7851bfac58f917a186235c7c4e8
phenix3443 <phenix3443@gmail.com>
2015-12-10 12:56:33 +0800

    add elpa

---
+++ elpa/chinese-pyim-20151130.1903/chinese-pyim-pkg.el
@@ -0,0 +1,8 @@
+(define-package "chinese-pyim" "20151130.1903" "Chinese pinyin input method"
+  '((cl-lib "0.5")
+    (pos-tip "0.4"))
+  :url "https://github.com/tumashu/chinese-pyim" :keywords
+  '("convenience" "chinese" "pinyin" "input-method"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
