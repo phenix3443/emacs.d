47e0b98e656e8d2b898dd11f270382c3fd194184
liushangliang <liushangliang@xunlei.com>
2015-12-14 19:12:47 +0800

    add

--- init.el
+++ init.el
@@ -200,28 +200,3 @@
 ;; Widget	Library for graphical widgets.
 ;; Wisent	An Emacs implementation of the GNU Compiler Compiler Bison.
 ;; Woman	Browsing UN*X pages without man.
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(helm-gtags-auto-update t)
- '(helm-gtags-direct-helm-completing t)
- '(helm-gtags-display-style (quote detail))
- '(helm-gtags-fuzzy-match nil)
- '(helm-gtags-highlight-candidate t)
- '(helm-gtags-ignore-case t)
- '(helm-gtags-prefix-key (kbd "C-c g"))
- '(helm-gtags-pulse-at-cursor t)
- '(helm-gtags-read-only nil)
- '(helm-gtags-update-interval-second 60)
- '(helm-gtags-use-input-at-cursor t)
- '(package-selected-packages
-   (quote
-	(jedi persp-projectile perspective helm-ag helm-projectile projectile paredit pangu-spacing org-page org2blog htmlize cdlatex multiple-cursors markdown-mode magit lua-mode indent-guide helm-gtags helm guide-key flycheck csharp-mode chinese-pyim fuzzy pos-tip auto-complete popup auto-compile auctex))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
