24af50d2c785118ad519e16914650f267c17598f
liushangliang <liushangliang@xunlei.com>
2015-12-15 14:22:30 +0800

    add

--- init.el
+++ init.el
@@ -184,6 +184,12 @@
 ;; Tramp	Transparent remote file access system.
 (require 'tramp)
 (setq tramp-default-user "phenix")
+(cond
+ ((eq system-type 'windows-nt)
+  (setq tramp-default-method "pscp"))
+ ((eq system-type 'gnu/linux)
+  (setq tramp-default-method "ssh")))
+
 (setq password-cache-expiry 86400)
 
 ;; URL	Library for manipulating and retrieving URLs and URIs.
