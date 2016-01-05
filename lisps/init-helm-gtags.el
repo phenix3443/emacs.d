c3b5ae2b93816230a7d47bd4f330281d70d416b9
liushangliang@xunlei.com <phenix@localhost.localdomain>
2016-01-05 17:07:19 +0800

    add some config

--- lisps/init-helm-gtags.el
+++ lisps/init-helm-gtags.el
@@ -54,17 +54,19 @@
 (add-hook 'prog-mode-hook 'helm-gtags-mode)
 
 (custom-set-variables
- '(helm-gtags-prefix-key (kbd "C-c g"))
+ '(helm-gtags-parse-file 'root)
  '(helm-gtags-ignore-case t)
- '(helm-gtags-read-only nil)
+ '(helm-gtags-read-only t)
  '(helm-gtags-use-input-at-cursor t)
  '(helm-gtags-highlight-candidate t)
  '(helm-gtags-display-style 'detail)
  '(helm-gtags-auto-update t)
  '(helm-gtags-update-interval-second 60)
+ '(helm-gtags-cache-select-result t)
  '(helm-gtags-pulse-at-cursor t)
  '(helm-gtags-fuzzy-match nil)
  '(helm-gtags-direct-helm-completing t)
+ '(helm-gtags-prefix-key (kbd "C-c g"))
 )
 
 ;; key bindings
