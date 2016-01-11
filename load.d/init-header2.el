400a99e1eab11a0267890804e63516267e949d1f
lsl-xunlei-ubuntu <liushangliang@xunlei.com>
2016-01-11 16:58:23 +0800

    fix elpa bug

--- load.d/init-header2.el
+++ load.d/init-header2.el
@@ -1,58 +1,11 @@
-;;; init-header2.el --- configure header2
-;;
-;; Filename: init-header2.el
-;; Description: header2 configuration
-;; Author: phenix3443 <phenix3443@gmail.com>
-;; Maintainer: phenix3443 <phenix3443@gmail.com>
-;; Created: 周四 十二月 18 16:03:28 2014 (+0800)
-;; Version: 1.0
-;; Package-Requires: (conf-package.el package.el)
-;; Last-Updated: 四 10月 22 00:41:34 2015 (+0800)
-;;           By: ubuntu
-;;     Update #: 7
-;; URL:
-;; Doc URL:
-;; Keywords: auto make file header
-;; Compatibility:
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;;; Commentary:
-;;
-;;
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;;; Change Log:
-;;
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or (at
-;; your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;;; Code:
-
-(require 'header2)
-;; update file headers when save a file
-(autoload 'auto-update-file-header "header2")
-(add-hook 'write-file-hooks 'auto-update-file-header)
-;; add a file headers when create a new file
-(autoload 'auto-make-header "header2")
-(add-hook 'emacs-lisp-mode-hook 'auto-make-header)
-
+(require 'req-package)
+(req-package header2
+  :config
+  ;; update file headers when save a file
+  (autoload 'auto-update-file-header "header2")
+  (add-hook 'write-file-hooks 'auto-update-file-header)
+  ;; add a file headers when create a new file
+  (autoload 'auto-make-header "header2")
+  (add-hook 'emacs-lisp-mode-hook 'auto-make-header)
+  )
 (provide 'init-header2)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; init-header2.el ends here
