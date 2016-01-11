34c1c8fbee86599b5cfef26589c891c42c841e9f
lsl-xunlei-ubuntu <liushangliang@xunlei.com>
2016-01-11 16:58:23 +0800

    fix elpa bug

--- load.d/init-guide-key.el
+++ load.d/init-guide-key.el
@@ -1,66 +1,20 @@
-;;; init-guide-key.el ---
-;;
-;; Filename: init-guide-key.el
-;; Description:
-;; Author: phenix3443 <phenix3443@gmail.com>
-;; Maintainer: phenix3443
-;; Created: 周五 7月 31 13:20:24 2015 (+0800)
-;; Version:
-;; Package-Requires: ()
-;; Last-Updated:
-;;           By:
-;;     Update #: 9
-;; URL:
-;; Doc URL:
-;; Keywords:
-;; Compatibility:
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;;; Commentary:
-;;
-;;
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;;; Change Log:
-;;
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or (at
-;; your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;;; Code:
-(require 'guide-key)
-(setq guide-key/guide-key-sequence '("C-x r"
-									 "C-x 4"
-									 "C-c p" ;projectile
-									 "C-c p s"
-									 "C-c h" ;helm
-									 "C-c g" ;gtags
-									 "C-c C-e"))
-;; (setq guide-key/recursive-key-sequence-flag t)
-(setq guide-key/highlight-command-regexp
-      '("rectangle"
-        ("register" . font-lock-type-face)
-        ("bookmark" . "hot pink")))
-(setq guide-key/idle-delay 0.1)
-(guide-key-mode 1)
-(setq guide-key/popup-window-position 'bottom)
-
+(require 'req-package)
+(req-package guide-key
+  :config
+  (setq guide-key/guide-key-sequence '("C-x r"
+									   "C-x 4"
+									   "C-c p" ;projectile
+									   "C-c p s"
+									   "C-c h" ;helm
+									   "C-c g" ;gtags
+									   "C-c C-e"))
+  ;; (setq guide-key/recursive-key-sequence-flag t)
+  (setq guide-key/highlight-command-regexp
+		'("rectangle"
+		  ("register" . font-lock-type-face)
+		  ("bookmark" . "hot pink")))
+  (setq guide-key/idle-delay 0.1)
+  (guide-key-mode 1)
+  (setq guide-key/popup-window-position 'bottom)
+  )
 (provide 'init-guide-key)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; init-guide-key.el ends here
--- load.d/init-paredit.el
+++ load.d/init-paredit.el
@@ -1,68 +1,21 @@
-;;; init-paredit.el ---
-;;
-;; Filename: init-paredit.el
-;; Description:
-;; Author: phenix3443<phenix3443@gmail.com>
-;; Maintainer: phenix3443
-;; Created: 周三 十二月 24 11:19:44 2014 (+0800)
-;; Version:
-;; Package-Requires: ()
-;; Last-Updated: 周四 10月 29 10:10:33 2015 (+0800)
-;;           By: phenix
-;;     Update #: 26
-;; URL:
-;; Doc URL:
-;; Keywords:
-;; Compatibility:
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;;; Commentary:
-;;
-;;
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;;; Change Log:
-;;
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or (at
-;; your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;;; Code:
+(require 'req-package)
+(req-package paredit
+  :config
 
-(require 'paredit)
+  (autoload 'enable-paredit-mode "paredit" t)
 
-(autoload 'enable-paredit-mode "paredit" t)
+  (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
 
-(add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+  (with-eval-after-load 'paredit
+	(define-key paredit-mode-map (kbd "C-)") 'paredit-forward-slurp-sexp)
+	(define-key paredit-mode-map (kbd "C-(") 'paredit-backward-slurp-sexp)
+	(define-key paredit-mode-map (kbd "C-}") 'paredit-forward-barf-sexp)
+	(define-key paredit-mode-map (kbd "C-{") 'paredit-backward-barf-sexp)
+	(define-key paredit-mode-map (kbd "C-<right>") nil)
+	(define-key paredit-mode-map (kbd "C-<left>")  nil))
 
-(with-eval-after-load 'paredit
-  (define-key paredit-mode-map (kbd "C-)") 'paredit-forward-slurp-sexp)
-  (define-key paredit-mode-map (kbd "C-(") 'paredit-backward-slurp-sexp)
-  (define-key paredit-mode-map (kbd "C-}") 'paredit-forward-barf-sexp)
-  (define-key paredit-mode-map (kbd "C-{") 'paredit-backward-barf-sexp)
-  (define-key paredit-mode-map (kbd "C-<right>") nil)
-  (define-key paredit-mode-map (kbd "C-<left>")  nil))
-
-;; (require-package 'paredit-everywhere)
-;; (add-hook 'prog-mode-hook 'paredit-everywhere-mode)
-;; (add-hook 'css-mode-hook 'paredit-everywhere-mode)
-
+  ;; (require-package 'paredit-everywhere)
+  ;; (add-hook 'prog-mode-hook 'paredit-everywhere-mode)
+  ;; (add-hook 'css-mode-hook 'paredit-everywhere-mode)
+  )
 (provide 'init-paredit)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; init-paredit.el ends here
