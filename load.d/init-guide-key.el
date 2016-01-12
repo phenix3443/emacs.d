1e0ddf1fdc4bb36e1be177528579fc480f0435b3
liushangliang <liushangliang@xunlei.com>
2016-01-12 10:27:36 +0800

    add helm keys

--- load.d/init-guide-key.el
+++ load.d/init-guide-key.el
@@ -1,13 +1,23 @@
 (require 'req-package)
 (req-package guide-key
   :config
-  (setq guide-key/guide-key-sequence '("C-x r"
-									   "C-x 4"
-									   "C-c p" ;projectile
-									   "C-c p s"
-									   "C-c h" ;helm
-									   "C-c g" ;gtags
-									   "C-c C-e"))
+  (setq guide-key/guide-key-sequence
+		'("C-x r"
+		  "C-x 4"
+		  ;; gtags
+		  "C-c g"
+		  ;; helm
+		  "C-c h"
+		  "C-c h C-c"
+		  "C-c h C-x"
+		  "C-c h h"
+		  "C-c h M-s"
+		  "C-c h M-g"
+		  ;; projectile
+		  "C-c p"
+		  "C-c p s"
+
+		  "C-c C-e"))
   ;; (setq guide-key/recursive-key-sequence-flag t)
   (setq guide-key/highlight-command-regexp
 		'("rectangle"
