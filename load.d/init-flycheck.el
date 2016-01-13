13262570d26970e15e30b7cf0e3f29b71f1041ae
liushangliang <liushangliang@xunlei.com>
2016-01-13 22:29:40 +0800

    fix floder layout

--- load.d/init-flycheck.el
+++ load.d/init-flycheck.el
@@ -1,6 +1,7 @@
 (require 'req-package)
 
 (req-package flycheck
+  :disable t
   :config
   (add-hook 'prog-mode-hook #'flycheck-mode)
   (setq flycheck-check-syntax-automatically '(mode-enabled save))
