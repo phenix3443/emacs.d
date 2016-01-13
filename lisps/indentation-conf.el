608795268630a21285f062219c335c77fc2e36c6
liushangliang <liushangliang@xunlei.com>
2016-01-13 22:25:39 +0800

    add

--- lisps/indentation-conf.el
+++ lisps/indentation-conf.el
@@ -1,55 +1,9 @@
-;;; indentation-conf.el ---
-;;
-;; Filename: indentation-conf.el
-;; Description:
-;; Author: phenix3443 <phenix3443@gmail.com>
-;; Maintainer: phenix3443
-;; Created: 周六 一月 17 10:10:24 2015 (+0800)
-;; Version:
-;; Package-Requires: ()
-;; Last-Updated: 一 11月  2 16:46:53 2015 (+0800)
-;;           By: ubuntu
-;;     Update #: 12
-;; URL:
-;; Doc URL:
-;; Keywords:
-;; Compatibility:
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;;; Commentary:
-;;
-;;
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;;; Change Log:
-;;
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or (at
-;; your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;;; Code:
 
-;;24.3 Tabs vs Spaces
+;; 24.3 Tabs vs Spaces
+(setq-default indent-tabs-mode t)
+
 (setq-default tab-width 4)
-(setq tab-always-indent 'complete)
-(electric-indent-mode t)
-(provide 'indentation-conf)
+;; 24.4 Convenience Features for Indentation
+(setq tab-always-indent nil)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; indentation-conf.el ends here
+(provide 'indentation-conf)
