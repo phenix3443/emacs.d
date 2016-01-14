d9909140a38e88f08072e7add2abbd95866ea09f
phenix3443 <phenix3443@gmail.com>
2016-01-15 01:04:57 +0800

    enable hideshow mode

--- lisps/program-conf.el
+++ lisps/program-conf.el
@@ -1,50 +1,3 @@
-;;; program-conf.el --- related to programming
-;;
-;; Filename: program-conf.el
-;; Description:
-;; Author: phenix3443 <phenix3443@gmail.com>
-;; Maintainer: phenix3443 <phenix3443@gmail.com>
-;; Created:
-;; Version:
-;; Package-Requires: ()
-;; Last-Updated: 周五 7月 31 12:59:05 2015 (+0800)
-;;           By: chengxu70
-;;     Update #: 31
-;; URL:
-;; Doc URL:
-;; Keywords:
-;; Compatibility:
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;;; Commentary:
-;;
-;;
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;;; Change Log:
-;;
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or (at
-;; your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;;; Code:
-
 ;; 26.1 major mode for programming languages
 ;; 26.4 Commands for Editing with Parentheses
 ;; 26.4.3 matching parentheses
@@ -70,12 +23,12 @@
 	(comment-or-uncomment-region start end)))
 
 (global-set-key (kbd "C-;") 'comment-dwim-line)
+;; 26.7 Hideshow minor mode
+(add-hook 'prog-mode 'hs-minor-mode)
 ;; 26.8 Completion for symbol names
 
 ;; 27.5 Finding Syntax Error on The Fly
 
-;;28 tags
+;; 28 tags
 
 (provide 'program-conf)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; program-conf.el ends here
