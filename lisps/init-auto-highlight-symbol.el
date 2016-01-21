0414430c29877f97b0b100edf3b94c89e4ce26a7
liushangliang <liushangliang@xunlei.com>
2016-01-21 16:33:12 +0800

    enable auto-highlight-symbol

--- lisps/init-auto-highlight-symbol.el
+++ lisps/init-auto-highlight-symbol.el
@@ -1,10 +1,8 @@
 ;; -*- coding:utf-8; -*-
-(require 'req-package)
 
-(req-package auto-highlight-symbol
-  :config
-  ;; (require-package 'auto-highlight-symbol)
-  (require 'auto-highlight-symbol)
-  (global-auto-highlight-symbol-mode t)
-  )
+(require-package 'auto-highlight-symbol)
+
+(global-auto-highlight-symbol-mode t)
+
+
 (provide 'init-auto-highlight-symbol)
