c450117b4dfe951b99f48768022eb24cd92a6d01
liushangliang <liushangliang@xunlei.com>
2016-01-22 18:28:10 +0800

    cedet

--- load.d/init-cedet.el
+++ load.d/init-cedet.el
@@ -19,54 +19,58 @@
   ;; configure from semantic manual
 
   ;; 2.1 Semantic mode
-  ;; (setq semantic-default-submodes
-  ;; 		'(global-semanticdb-minor-mode
-  ;; 		  global-semantic-decoration-mode
-  ;; 		  ;;global-semantic-highlight-func-mode
-  ;; 		  global-semantic-idle-local-symbol-highlight-mode
-  ;; 		  global-semantic-idle-scheduler-mode
-  ;; 		  global-semantic-idle-summary-mode
-  ;; 		  global-semantic-idle-completions-mode
-  ;; 		  ;;global-semantic-mru-bookmark-mode
-  ;; 		  ;;global-semantic-stickyfunc-mode
-  ;; 		  ))
+  (setq semantic-default-submodes
+  		'(global-semanticdb-minor-mode
+  		  global-semantic-decoration-mode
+  		  ;;global-semantic-highlight-func-mode
+  		  global-semantic-idle-local-symbol-highlight-mode
+  		  global-semantic-idle-scheduler-mode
+  		  global-semantic-idle-summary-mode
+  		  global-semantic-idle-completions-mode
+  		  ;;global-semantic-mru-bookmark-mode
+  		  ;;global-semantic-stickyfunc-mode
+  		  ))
   ;; 2.2.1 Semanticdb Tag Storage
-  ;; (setq semantic-default-save-directory
-  		;; "~/.emacs.d/tmp-dir/semanticdb/")
+  (setq semantic-default-save-directory
+  		"~/.emacs.d/tmp-dir/semanticdb/")
 
   ;; global support
 
-  ;; (setq cedet-global-command "global")
-  ;; (when(cedet-gnu-global-version-check t)
-  ;; 	(semanticdb-enable-gnu-global-databases 'c-mode t)
-  ;; 	(semanticdb-enable-gnu-global-databases 'c++-mode t))
-
+  (setq cedet-global-command "global")
+  (when(cedet-gnu-global-version-check t)
+  	(semanticdb-enable-gnu-global-databases 'c-mode t)
+  	(semanticdb-enable-gnu-global-databases 'c++-mode t))
 
   ;; 2.2.2.2 SemanticDB project roots
-  ;; (defun get-semantic-project-root()
-  ;; 	(let((semantic-projct-root-markers . '(".git" ".svn" "GTAGS" "TAGS")))
-  ;; 	  ))
-  ;; (add-hook 'semanticdb-project-root-functions 'projectile-project-root)
+  (defun get-semantic-project-root()
+  	(let((semantic-projct-root-markers . '(".git" ".svn" "GTAGS" "TAGS")))
+  	  ))
+  (add-hook 'semanticdb-project-root-functions 'projectile-project-root)
 
-  ;; ;; 2.2.2.3 Include Paths
-  ;; (defvar user-include-dirs
-  ;; 	(list "." "./include" ".." "../include" "/opt/lixian/include" "/opt/lixian/include/appframe/"))
-  ;; (defvar win-include-dirs
-  ;; 	(list "C:/MinGW/include" "C:/msys64/usr/include" "C:/Program Files (x86)/Microsoft Visual Studio 11.0/VC/include"))
-  ;; (require 'semantic/bovine/c)
+  ;; 2.2.2.3 Include Paths
+  (defvar user-include-dirs
+  	(list "." "./include" ".." "../include" "/opt/lixian/include" "/opt/lixian/include/appframe/"))
+  (defvar win-include-dirs
+  	(list "C:/MinGW/include" "C:/msys64/usr/include" "C:/Program Files (x86)/Microsoft Visual Studio 11.0/VC/include"))
+  (require 'semantic/bovine/c)
 
-  ;; (let ((include-dirs user-include-dirs))
-  ;; 	(when (eq system-type 'window-nt)
-  ;; 	  (semantic-reset-system-include 'c-mode)
-  ;; 	  (semantic-reset-system-include 'c++-mode)
-  ;; 	  (append include-dirs win-include-dirs))
-  ;; 	(mapc (lambda (dir)
-  ;; 			(semantic-add-system-include dir 'c-mode)
-  ;; 			(semantic-add-system-include dir 'c++-mode))
-  ;; 		  include-dirs))
+  (let ((include-dirs user-include-dirs))
+  	(when (eq system-type 'window-nt)
+  	  (semantic-reset-system-include 'c-mode)
+  	  (semantic-reset-system-include 'c++-mode)
+  	  (append include-dirs win-include-dirs))
+  	(mapc (lambda (dir)
+  			(semantic-add-system-include dir 'c-mode)
+  			(semantic-add-system-include dir 'c++-mode))
+  		  include-dirs))
 
   (semantic-mode 1)
-  ;(semantic-load-enable-minimum-features)
+
+  ;; Do not install ECB before you have successfully installed the requirements!
+  ;(use-package ecb
+	;:disabled t
+	;:ensure t
+	;)
   )
 
 (provide 'init-cedet)
