df2a40c46c00859410b27e6b2fd9df0066a8c68b
liushangliang <liushangliang@xunlei.com>
2016-02-18 17:21:35 +0800

    add

--- lisps/indentation-conf.el
+++ lisps/indentation-conf.el
@@ -3,6 +3,7 @@
 (setq-default indent-tabs-mode t)
 
 (setq-default tab-width 4)
+(setq tab-stop-list (number-sequence 4 120 4))
 ;; 24.4 Convenience Features for Indentation
 (setq tab-always-indent nil)
 
