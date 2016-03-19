058af618301ca07cf9808e97ececec6f06361b2f
phenix3443 <phenix3443@gmail.com>
2016-03-20 01:12:44 +0800

    解决org与fci-mode冲突产生方块问题

--- load.d/init-fill-column-indicator.el
+++ load.d/init-fill-column-indicator.el
@@ -7,4 +7,12 @@
   :config
   (require 'fill-column-indicator)
   (add-hook 'prog-mode-hook (lambda () (fci-mode 1)))
+
+  (defun fci-mode-override-advice (&rest args))
+  (advice-add 'org-html-fontify-code :around
+			  (lambda (fun &rest args)
+				(advice-add 'fci-mode :override #'fci-mode-override-advice)
+				(let ((result  (apply fun args)))
+				  (advice-remove 'fci-mode #'fci-mode-override-advice)
+				  result)))
   )
