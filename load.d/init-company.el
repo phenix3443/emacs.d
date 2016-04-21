6e03f4201377de8efa7a42c1d7bd50115688081f
liushangliang <liushangliang@xunlei.com>
2016-04-21 21:24:08 +0800

    启用company进行补全，并添加一些模块

--- load.d/init-company.el
+++ load.d/init-company.el
@@ -4,28 +4,51 @@
 
 (use-package company
   :ensure t
-  :disabled t
+  ;; :disabled t
   :config
   (setq company-idle-delay 0.1)
   (setq company-minimum-prefix-length 2)
 
-  (setq company-backends
-		'(company-gtags
-		  company-yasnippet
-		  company-files
-		  company-dabbrev
-		  company-dabbrev-code
-		  company-etags
-		  company-semantic
-		  company-elisp
-		  ))
+  (setq company-backends (append '(company-yasnippet company-elisp) company-backends))
 
-  (require-package 'company-c-headers)
-  (require 'company-c-headers)
-  (add-to-list 'company-c-headers-path-system "usr/include/c++")
-  (add-to-list 'company-backends 'company-c-headers)
+  (use-package helm-company
+	:ensure t
+	:disabled t
+	:config)
 
-  (add-hook 'prog-mode-hook '(lambda()
-							   (company-mode t)))
+  (use-package company-c-headers
+	:ensure t
+	:config
+	(require 'company-c-headers)
+	;; (add-to-list 'company-c-headers-path-system "/usr/include/c++")
+	(add-to-list 'company-backends 'company-c-headers)
+	)
+
+
+  (use-package company-lua
+	:ensure t
+	:config
+	(require 'company-lua)
+	(add-to-list 'company-backends 'company-lua)
+	)
+
+  (use-package company-quickhelp
+	:ensure t
+	;; :disabled t
+	:config
+	(company-quickhelp-mode 1)
+	(eval-after-load 'company
+	  '(define-key company-active-map (kbd "M-h") #'company-quickhelp-manual-begin))
+	)
+
+  (use-package company-ycmd
+	:ensure t
+	:disabled t
+	:config
+	(company-ycmd-setup)
+	)
+
+
+  (add-hook 'after-init-hook 'global-company-mode)
   )
 (provide 'init-company)
