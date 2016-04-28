cc8e57fa7fee486b6938e9bb2f228f7041864c55
liushangliang <liushangliang@xunlei.com>
2016-04-28 15:36:26 +0800

    重新调整了 company 不全后端的设置，以及正对各中语言的 hook

--- load.d/init-irony.el
+++ load.d/init-irony.el
@@ -18,6 +18,7 @@
       'irony-completion-at-point-async)
     (define-key irony-mode-map [remap complete-symbol]
       'irony-completion-at-point-async))
+
   (add-hook 'irony-mode-hook 'my-irony-mode-hook)
   (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
   )
