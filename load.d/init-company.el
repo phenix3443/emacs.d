1ceb48298b20cdce8356da41d2ea784dea25e73f
liushangliang <liushangliang@xunlei.com>
2016-04-28 15:36:26 +0800

    重新调整了 company 不全后端的设置，以及正对各中语言的 hook

--- load.d/init-company.el
+++ load.d/init-company.el
@@ -7,52 +7,25 @@
   :defer t
   :init (add-hook 'after-init-hook 'global-company-mode)
   :config
-  (setq company-idle-delay .3)
-  (setq company-echo-delay 0)
-  (setq company-begin-commands '(self-insert-command))
 
-  ;; (setq company-minimum-prefix-length 2)
+  (setq company-minimum-prefix-length 1)
+
   ;; :bind ("C-;" . company-complete-common)
-  (defun company-prog-mode-setup ()
-	(add-to-list 'company-backends 'company-gtags)
-	(add-to-list 'company-backends 'company-yasnippet)
-	)
 
-  (add-hook 'prog-mode-hook 'company-prog-mode-setup)
-
-  (defun compnay-c-mode-setup ()
-	(add-to-list 'company-backends 'company-irony)
-	)
-
-  (add-hook 'c-mode-hook 'company-c-mode-setup)
-
-
-  (defun company-elisp-mode-setup ()
-	(add-to-list 'company-backends 'company-elisp)
-	)
-
-  (add-hook 'elisp-mode-hook 'company-elisp-mode-setup)
-
-  (use-package company-c-headers
-  	:ensure t
-  	:disabled t
-  	:config
-  	(require 'company-c-headers)
-	(add-to-list 'company-c-headers-path-system "/usr/include/c++")
-	(defun company-c-header-setup ()
-	  (add-to-list 'company-backends 'company-c-headers))
-	(add-hook 'c-mode-hook 'company-c-header-setup)
-  	)
-
+  ;; (use-package company-c-headers
+  ;; 	:ensure t
+  ;; 	:disabled t
+  ;; 	:config
+  ;; 	(require 'company-c-headers)
+  ;; 	(add-to-list 'company-c-headers-path-system "/usr/include/c++")
+  ;; 	(defun company-c-header-setup ()
+  ;; 	  (add-to-list 'company-backends 'company-c-headers))
+  ;; 	(add-hook 'c-mode-hook 'company-c-header-setup)
+  ;; 	)
 
   (use-package company-lua
   	:ensure t
   	:disabled t
-  	:config
-  	(require 'company-lua)
-	(defun company-lua-mode-setup ()
-	  (add-to-list 'company-backends 'company-lua))
-	(add-hook 'lua-mode-hook 'company-lua-mode-setup)
   	)
 
   (use-package company-quickhelp
@@ -72,12 +45,10 @@
   	(company-ycmd-setup)
   	)
 
-  (use-package company-irony-c-headers	:ensure t
+  (use-package company-irony-c-headers
+	:ensure t
 	;; :disabled t
 	:config
-	(eval-after-load 'company
-	  '(add-to-list
-		'company-backends 'company-irony-c-headers))
 	)
 
   (use-package company-irony
@@ -85,9 +56,36 @@
 	;; :disabled t
 	:defer t
 	:config
-	(eval-after-load 'company
-	  '(add-to-list 'company-backends 'company-irony))
 	)
+  (use-package company-jedi
+	:ensure t
+	:config
+	)
+
+  (defun company-c-mode-setup ()
+	(set (make-local-variable 'company-backends) '((company-irony-c-headers  company-irony company-yasnippet company-dabbrev company-dabbrev-code company-keywords company-gtags company-files)) ))
+
+
+  (add-hook 'c-mode-hook 'company-c-mode-setup)
+  (add-hook 'c++-mode-hook 'company-c-mode-setup)
+
+
+  (defun company-elisp-mode-setup ()
+	(set (make-local-variable 'company-backends) '((company-elisp company-dabbrev company-dabbrev-code company-yasnippet company-gtags company-files))))
+
+  (add-hook 'emacs-lisp-mode-hook 'company-elisp-mode-setup)
+
+  (defun company-lua-mode-setup ()
+	(set (make-local-variable 'company-backends) '((company-lua company-dabbrev company-dabbrev-code company-gtags company-files))))
+
+  (add-hook 'lua-mode-hook 'company-lua-mode-setup)
+
+
+  (defun company-python-mode-hook ()
+		(set (make-local-variable 'company-backends) '((company-jedi company-dabbrev company-dabbrev-code company-gtags company-files))))
+
+  (add-hook 'python-mode-hook 'company-python-mode-hook)
+
   )
 
 (provide 'init-company)
