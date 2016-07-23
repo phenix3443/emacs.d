433f6b7e52eb811524cf244dee2b51a14c778ef6
phenix3443 <phenix3443@gmail.com>
2016-07-23 12:10:37 +0800

    修正 org-note 推送位置

--- load.d/init-org.el
+++ load.d/init-org.el
@@ -82,7 +82,7 @@
 		  ("org"
 		   ;; 13.1.2 Sources and destinations for files
 		   :base-directory "~/github/org-notes/org/" ;设置存放.org 文件位置
-		   :publishing-directory "~/github/org-notes/phenix3443.github.io/" ;导出 html 文件位置
+		   :publishing-directory "~/github/phenix3443.github.io/" ;导出 html 文件位置
 		   ;; :preparation-function
 		   ;; :completion-function
 
@@ -111,7 +111,7 @@
 		  ("static"
 		   :base-directory "~/github/org-notes/org/"
 		   :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-		   :publishing-directory "~/github/org-notes/phenix3443.github.io/"
+		   :publishing-directory "~/github/phenix3443.github.io/"
 		   :recursive t
 		   :publishing-function org-publish-attachment
 		   )
@@ -122,11 +122,11 @@
   (when (equal system-type 'windows-nt)
 	(setq org-plist (cdr (assoc "org" org-publish-project-alist)))
 	(plist-put org-plist :base-directory "D:/github/org-notes/org/")
-	(plist-put org-plist :publishing-directory "D:/github/org-notes/phenix3443.github.io/")
+	(plist-put org-plist :publishing-directory "D:/github/phenix3443.github.io/")
 
 	(setq static-plist (cdr (assoc "static" org-publish-project-alist)))
 	(plist-put static-plist :base-directory "D:/github/org-notes/org/")
-	(plist-put static-plist :publishing-directory "D:/github/org-notes/phenix3443.github.io/"))
+	(plist-put static-plist :publishing-directory "D:/github/phenix3443.github.io/"))
 
   ;; (setq org-babel-default-header-args
   ;; (cons '(:base-directory "D:/projects/org-notes/org/")
