c1fade7be1c10aa5d53b9dd141f76f87aae75370
phenix3443 <phenix3443@gmail.com>
2016-08-06 13:26:00 +0800

    文件名添加章节序号

---
+++ lisps/43_saving_emacs_sessions.el
@@ -0,0 +1,4 @@
+;; -*- coding:utf-8; -*-
+
+;;42 saving emacs sessions
+(desktop-save-mode 1)
---
+++ lisps/7_basic_editing_commands.el
@@ -0,0 +1,6 @@
+;; -*- coding:utf-8; -*-
+
+;; 7.2 change the location of point
+(setq track-eol t)
+
+(provide 'basic-edit-conf)
