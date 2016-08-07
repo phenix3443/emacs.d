47f9c6340c0bf12dabf3e2702f056ce1aba00b40
phenix3443 <phenix3443@gmail.com>
2016-08-07 14:06:43 +0800

    修改文件名

--- lisps/48_customization.el
+++ lisps/48_customization.el
@@ -1,7 +1,7 @@
 ;; -*- coding:utf-8; -*-
 
 ;; 48.1.4 Saving Customizations
-(setq custom-file "~/.emacs.d/emacxs-custom.el")
+(setq custom-file "~/.emacs.d/emacs-custom.el")
 
 ;; 48.1.7 custom themes
 
--- load.d/moe_theme_conf.el
+++ load.d/moe_theme_conf.el
@@ -4,24 +4,22 @@
 
 (use-package moe-theme
   :ensure t
+  :init
+  (require 'powerline)
   :config
-
   ;; Show highlighted buffer-id as decoration. (Default: nil)
-    (setq moe-theme-highlight-buffer-id t)
-    ;; Resize titles (optional).
-    (setq moe-theme-resize-markdown-title '(1.5 1.4 1.3 1.2 1.0 1.0))
-    (setq moe-theme-resize-org-title '(1.5 1.4 1.3 1.2 1.1 1.0 1.0 1.0 1.0))
-    (setq moe-theme-resize-rst-title '(1.5 1.4 1.3 1.2 1.1 1.0))
+  (setq moe-theme-highlight-buffer-id t)
+  ;; Resize titles (optional).
+  (setq moe-theme-resize-markdown-title '(1.5 1.4 1.3 1.2 1.0 1.0))
+  (setq moe-theme-resize-org-title '(1.5 1.4 1.3 1.2 1.1 1.0 1.0 1.0 1.0))
+  (setq moe-theme-resize-rst-title '(1.5 1.4 1.3 1.2 1.1 1.0))
 
-    ;; Choose a color for mode-line.(Default: blue)
-	;; (Available colors: blue, orange, green ,magenta, yellow, purple, red, cyan, w/b.)
-    (moe-theme-set-color 'orange)
+  ;; (moe-dark)
+  ;; (moe-light)
+  (require 'moe-theme-switcher)
 
-	(require 'powerline)
-	(powerline-moe-theme)
-
-    ;; Finally, apply moe-theme now.
-    ;; Choose what you like, (moe-light) or (moe-dark)
-    (moe-dark)
-	)
+  ;; Choose a color for mode-line.(Default: blue)
+  (moe-theme-set-color 'orange)
+  ;; (powerline-moe-theme)
+  )
 (provide 'moe_theme_conf)
