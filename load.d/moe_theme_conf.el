99f49addb49cfcb1426bac450b5b48cbc5017691
liushangliang <liushangliang@xunlei.com>
2016-08-08 12:38:40 +0800

    enable dark theme

--- load.d/moe_theme_conf.el
+++ load.d/moe_theme_conf.el
@@ -14,9 +14,9 @@
   (setq moe-theme-resize-org-title '(1.5 1.4 1.3 1.2 1.1 1.0 1.0 1.0 1.0))
   (setq moe-theme-resize-rst-title '(1.5 1.4 1.3 1.2 1.1 1.0))
 
-  ;; (moe-dark)
+  (moe-dark)
   ;; (moe-light)
-  (require 'moe-theme-switcher)
+  ;; (require 'moe-theme-switcher)
 
   ;; Choose a color for mode-line.(Default: blue)
   (moe-theme-set-color 'orange)
