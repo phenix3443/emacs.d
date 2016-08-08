21c0dfb140698c7a368d275e5c3921d251e1a448
liushangliang <liushangliang@xunlei.com>
2016-08-08 12:38:54 +0800

    enable which func mode

--- lisps/26_editing_programs.el
+++ lisps/26_editing_programs.el
@@ -1,6 +1,8 @@
 ;; -*- coding:utf-8; -*-
 
 ;; 26.1 major mode for programming languages
+;; 26.2.4 Which Function Mode
+(which-function-mode 1)
 ;; 26.4 Commands for Editing with Parentheses
 ;; 26.4.3 matching parentheses
 (show-paren-mode t)
