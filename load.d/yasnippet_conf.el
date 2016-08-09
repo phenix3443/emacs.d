b4a7f4ce7bc50111ff08b3db78f415c3fe425a56
liushangliang <liushangliang@xunlei.com>
2016-08-09 16:53:33 +0800

    change with use-package

--- load.d/yasnippet_conf.el
+++ load.d/yasnippet_conf.el
@@ -4,13 +4,15 @@
 
 (use-package yasnippet
   :ensure t
+  :bind (
+		 :map yas-minor-mode-map
+			  ;; Remove Yasnippet's default tab key binding
+			  ("TAB" . nil)
+			  ("<tab>" . nil)
+			  ;; Set Yasnippet's key binding to shift+tab
+			  ("<backtab>" . yas-expand))
   :config
-  ;; Remove Yasnippet's default tab key binding
-  (define-key yas-minor-mode-map (kbd "<tab>") nil)
-  (define-key yas-minor-mode-map (kbd "TAB") nil)
-  ;; Set Yasnippet's key binding to shift+tab
-  (define-key yas-minor-mode-map (kbd "<backtab>") 'yas-expand)
 
   (yas-global-mode 1)
   )
-(provide 'init-yasnippet)
+(provide 'yasnippet_conf)
