1375a742cba14f4d9ddde509ef8d72ef5c507a3e
liushangliang <liushangliang@xunlei.com>
2017-04-01 15:41:00 +0800

    add new

---
+++ load.d/clang_format_conf.el
@@ -0,0 +1,11 @@
+;; -*- coding:utf-8; -*-
+
+(require 'use-package)
+
+(use-package clang-format
+  :ensure t
+  :init
+  :config
+  ;; (add-hook 'c++-mode-hook (lambda ()(setq clang-format-style-option "llvm")))
+
+  )
