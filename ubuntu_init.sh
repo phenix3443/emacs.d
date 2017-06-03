fd22aa78a615444c8b10378d0613ac6d0089bbb9
phenix3443 <phenix3443@gmail.com>
2017-06-03 12:02:11 +0800

    删除了部分无用的配置，调整了配置

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -2,29 +2,17 @@
 
 CUR_DIR=$(cd `dirname $0`;pwd)
 
-
-# for cedet ycmd
-
-git submodule update --init --recursive
-
-# cedet
-cd ${CUR_DIR}/3rd-party/cedet-git
-make
-cd ${CUR_DIR}
-
-# Exuberant Ctags
-sudo apt-get install -y exuberant-ctags silversearcher-ag dos2unix
-
-# pygment
-sudo apt-get install -y python python-pip python-dev
-sudo pip install pygments
-
+sudo apt-get install -y silversearcher-ag dos2unix
 
 # GNU global
 sudo apt-get -y install global
+# Exuberant Ctags and pygments for global plugin
+sudo apt-get install -y exuberant-ctags python-pygments
 
+echo "# gnu global env start" >> ~/.bashrc
 echo "export GTAGSCONF=/usr/local/share/gtags/gtags.conf" >> ~/.bashrc
 echo "export GTAGSLABEL=pygments" >> ~/.bashrc
+echo "# gnu global evn end" >> ~/.bashrc
 
 # for jedi
 sudo apt-get install -y python python-dev
@@ -34,11 +22,5 @@ sudo pip install --upgrade virtualenv
 # for ac-lang
 sudo apt-get install -y  clang llvm libclang-dev
 
-# ycmd
-# sudo apt-get install build-essential cmake python-dev clang llvm libclang-dev
-# cd ${CUR_DIR}/3rd-party/ycmd/
-# ./build.py --all
-# cd ${CUR_DIR}
-
-echo Done successfully!
-echo Restart emacs and wait a moment for installing dependency packages from ELPA
+echo Prepare work has Done successfully!
+echo Start emacs and wait a moment for installing packages from ELPA
