51e2276439613ce15771be02f507c1f1815946ed
phenix3443 <phenix3443@gmail.com>
2017-09-15 00:32:38 +0800

    添加go相关的几个mode

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -13,6 +13,11 @@ echo "export PATH=$PATH:$GOROOT/bin:$HOME/gospace/bin" >> ~/.profile
 
 go get -u github.com/nsf/gocode
 go get -u github.com/rogpeppe/godef
+go get -u github.com/golang/lint/golint
+go get -u github.com/kisielk/errcheck
+go get -u github.com/mdempsky/unconvert
+go get -u golang.org/x/tools/cmd/guru
+
 # for lua
 sudo apt install luarocks
 sudo luarocks install luacheck
