dea121b7183e4999c208f5875b1f8ad1532bfad1
liushangliang <liushangliang@xunlei.com>
2018-07-02 16:28:40 +0800

    优化代码

--- lisps/theme-cfg.el
+++ lisps/theme-cfg.el
@@ -7,21 +7,7 @@
 (use-package moe-theme
   :ensure t
   :config
-  ;; (load-theme 'moe-dark)
-  ;; (add-to-list 'custom-theme-load-path (get-package-install-path "moe-theme"))
-  (setq moe-theme-highlight-buffer-id t)
-
-  (if (package-installed-p 'markdown-mode)
-      (setq moe-theme-resize-markdown-title '(1.5 1.4 1.3 1.2 1.0 1.0)))
-
-  (if (package-installed-p 'org)
-      (setq moe-theme-resize-org-title '(1.5 1.4 1.3 1.2 1.1 1.0 1.0 1.0 1.0)))
-
-  ;; (setq moe-theme-resize-rst-title '(1.5 1.4 1.3 1.2 1.1 1.0))
-  ;; (if (package-installed-p 'powerline)
-  ;;     (powerline-moe-theme))
-
-  (moe-dark)
+  (load-theme 'moe-dark t)
   )
 
 (use-package diminish
