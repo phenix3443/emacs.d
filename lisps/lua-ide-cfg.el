c6a596209837ffe4bd994160b47c8cc3b96a6cdf
liushangliang <phenix3443@gmail.com>
2018-08-25 19:37:19 +0800

    重新设置lua-mode代码

--- lisps/lua-ide-cfg.el
+++ lisps/lua-ide-cfg.el
@@ -3,14 +3,29 @@
 ;;; commentary:
 
 ;;; code:
+(defun company-lua-mode-setup()
+  "create lua company backend"
+  (set (make-local-variable 'company-backends)
+       (list (append '(company-lua company-lsp) (car company-backends)))))
 
 (use-package lua-mode
   :ensure t
   :mode "\\.lua$"
   :interpreter "lua"
+  :hook (lua-mode . company-lua-mode-setup)
+  :bind (:map lua-mode-map
+              ("C-c l b" . lua-send-buffer)
+              ("C-c l d" . lua-send-defun)
+              ("C-c l f" . lua-search-documentation)
+              ("C-c l h" . lua-hide-process-buffer)
+              ("C-c l l" . lua-send-current-line)
+              ("C-c l r" . lua-sedn-region)
+              ("C-c l s" . lua-show-process-buffer)
+              ("C-c l w" . lua-restart-with-whole-file))
   :config
   (setq lua-indent-level 4)
   (setq lua-indent-string-contents t)
+  (setq lua-prefix-key nil)
   )
 
 (use-package company-lua
@@ -18,13 +33,6 @@
   :ensure t
   :config
   )
-
-(defun company-lua-mode-setup()
-  "create lua company backend"
-  (set (make-local-variable 'company-backends)
-       (list (append '(company-lua company-lsp) (car company-backends)))))
-
-(add-hook 'lua-mode-hook 'company-lua-mode-setup)
 
 (provide 'lua-ide-cfg)
 ;;; lua-ide-cfg.el ends here
