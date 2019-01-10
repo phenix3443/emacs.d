5f7229a0f39250927eaa7437ca98f89cf20f6c28
liushangliang <phenix3443@gmail.com>
2019-01-11 00:53:39 +0800

    配置软件包

--- init.el
+++ init.el
@@ -5,13 +5,29 @@
 ;;; code:
 
 (require 'package)
-(setq package-enable-at-startup nil)
-(setq package-archives '(
-                         ("gnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
-                         ("Marmalade" ."http://mirrors.tuna.tsinghua.edu.cn/elpa/marmalade/")
-                         ("Org" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
-                         ))
+;; (setq package-enable-at-startup nil)
+
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl
+    (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  (add-to-list 'package-archives (cons "tsinghua-gnu" (concat proto "://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")) t)
+  (add-to-list 'package-archives (cons "tsinghua-melpa" (concat proto "://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")) t)
+  (add-to-list 'package-archives (cons "tsinghua-marmalade" (concat proto "://mirrors.tuna.tsinghua.edu.cn/elpa/marmalade/")) t)
+  (add-to-list 'package-archives (cons "tsinghua-org" (concat proto "://mirrors.tuna.tsinghua.edu.cn/elpa/org/")) t)
+
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
 (package-initialize)
 
 (add-to-list 'load-path (expand-file-name (concat user-emacs-directory "lisps")))
