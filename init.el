a012b8ab5ca368a1b50aa3c6f0fce67c80a8bea9
liushangliang <phenix3443@gmail.com>
2019-01-11 23:21:22 +0800

    还是使用清华源速度更快

--- init.el
+++ init.el
@@ -23,7 +23,7 @@ There are two things you can do about this warning:
   (add-to-list 'package-archives (cons "tsinghua-org" (concat proto "://mirrors.tuna.tsinghua.edu.cn/elpa/org/")) t)
 
   ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;; (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
   ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
   (when (< emacs-major-version 24)
     ;; For important compatibility libraries like cl-lib
