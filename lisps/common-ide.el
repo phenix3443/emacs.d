bdff79f751c74af3268b2fd1812bf084d15c0f61
phenix3443 <phenix3443@gmail.com>
2019-01-14 15:33:07 +0800

    添加lsp-ui按键绑定

--- lisps/common-ide.el
+++ lisps/common-ide.el
@@ -70,7 +70,8 @@
   :ensure t
   :after lsp-mode
   :config
-
+  (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
   )
 
 (use-package company-lsp
