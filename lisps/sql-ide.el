3463ad361c222ed5ace5535b34380490f96af2de
phenix3443 <phenix3443@gmail.com>
2019-01-22 21:08:55 +0800

    配置 sqlmode

--- lisps/sql-ide.el
+++ lisps/sql-ide.el
@@ -1,24 +1,28 @@
-;;; package --- summary  sql-ide配置
+;;; package --- summary  sql-ide 配置
 ;;; -*-coding:utf-8 -*-
 ;;; commentary:
 
 ;;; code:
 
-(use-package sql-indent
-  :ensure t
+(use-package sql-mode
   :mode "\\.sql$"
-  :hook sql
   :config
-  ;; (setq sql-indent-offset 4)
-  (setq sql-indent-debug t)
   )
 
+;; (use-package sqlformat
+;;   :ensure t
+;;   :hook (sql-mode . sqlformat-on-save-mode)
+;;   :config
+;;   (custom-set-default sqlformat-command )
+;; )
 
 (use-package sqlup-mode
   :ensure t
-  :hook (sql sql-interactive)
+  :diminish
+  :hook (sql-mode . sqlup-mode)
   :config
-  )
+)
+
 
 (provide 'sql-ide)
 ;;; sql-ide.el ends here
