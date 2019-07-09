dc42f4f5259c4be3e265c3b6dfff3dff38141a45
phenix3443 <phenix3443@gmail.com>
2019-07-09 20:57:38 +0800

    添加 ccls 配置相关内容

--- lisps/cxx-ide.el
+++ lisps/cxx-ide.el
@@ -70,9 +70,61 @@
   :ensure t
   ;; :hook ((c-mode c++-mode objc-mode) .
   ;;        (lambda () (require 'ccls) (lsp))))
+  :after(lsp lsp-ui projectile)
   :config
   ;; (setq lsp-prefer-flymake nil)
   ;; (setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
+
+  ;; Cross reference extensions
+  ;; direct callers
+  (lsp-find-custom "$ccls/call")
+  ;; callers up to 2 levels
+  (lsp-find-custom "$ccls/call" '(:levels 2))
+  ;; direct callees
+  (lsp-find-custom "$ccls/call" '(:callee t))
+
+  (lsp-find-custom "$ccls/vars")
+  ;; Use lsp-goto-implementation or lsp-ui-peek-find-implementation (textDocument/implementation) for derived types/functions
+  ;; $ccls/inheritance is more general
+
+  ;; Alternatively, use lsp-ui-peek interface
+  (lsp-ui-peek-find-custom "$ccls/call")
+  (lsp-ui-peek-find-custom "$ccls/call" '(:callee t))
+
+  ;; Recommended helpers:
+  (defun ccls/callee () (interactive) (lsp-ui-peek-find-custom "$ccls/call" '(:callee t)))
+  (defun ccls/caller () (interactive) (lsp-ui-peek-find-custom "$ccls/call"))
+  (defun ccls/vars (kind) (lsp-ui-peek-find-custom "$ccls/vars" `(:kind ,kind)))
+  (defun ccls/base (levels) (lsp-ui-peek-find-custom "$ccls/inheritance" `(:levels ,levels)))
+  (defun ccls/derived (levels) (lsp-ui-peek-find-custom "$ccls/inheritance" `(:levels ,levels :derived t)))
+  (defun ccls/member (kind) (interactive) (lsp-ui-peek-find-custom "$ccls/member" `(:kind ,kind)))
+
+  ;; References w/ Role::Role
+  (defun ccls/references-read () (interactive)
+         (lsp-ui-peek-find-custom "textDocument/references"
+                                  (plist-put (lsp--text-document-position-params) :role 8)))
+
+  ;; References w/ Role::Write
+  (defun ccls/references-write ()
+    (interactive)
+    (lsp-ui-peek-find-custom "textDocument/references"
+                             (plist-put (lsp--text-document-position-params) :role 16)))
+
+  ;; References w/ Role::Dynamic bit (macro expansions)
+  (defun ccls/references-macro () (interactive)
+         (lsp-ui-peek-find-custom "textDocument/references"
+                                  (plist-put (lsp--text-document-position-params) :role 64)))
+
+  ;; References w/o Role::Call bit (e.g. where functions are taken addresses)
+  (defun ccls/references-not-call () (interactive)
+         (lsp-ui-peek-find-custom "textDocument/references"
+                                  (plist-put (lsp--text-document-position-params) :excludeRole 32)))
+
+  ;; References whose filenames are under this project
+  (lsp-ui-peek-find-references nil (list :folders (vector (projectile-project-root))))
+
+  ;; ;; Semantic highlighting
+  (ccls-use-default-rainbow-sem-highlight)
   )
 
 (defun set-company-backends-for-cxx()
