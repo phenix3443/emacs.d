e470443b4e542a93437e8a2a51d431ad749e7b2e
liushangliang <liushangliang@xunlei.com>
2019-12-02 11:15:29 +0800

    全面使用清华的源

--- init.el
+++ init.el
@@ -17,10 +17,13 @@ which is unsafe because it allows man-in-the-middle attacks.
 There are two things you can do about this warning:
 1. Install an Emacs version that does support SSL and be safe.
 2. Remove this warning from your init file so you won't see it again."))
-  (add-to-list 'package-archives (cons "tsinghua-gnu" (concat proto "://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")) t)
-  (add-to-list 'package-archives (cons "tsinghua-melpa" (concat proto "://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")) t)
-  (add-to-list 'package-archives (cons "tsinghua-marmalade" (concat proto "://mirrors.tuna.tsinghua.edu.cn/elpa/marmalade/")) t)
-  (add-to-list 'package-archives (cons "tsinghua-org" (concat proto "://mirrors.tuna.tsinghua.edu.cn/elpa/org/")) t)
+  (setq package-archives
+        `(
+          (cons "tsinghua-gnu" (concat proto "://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/"))
+          (cons "tsinghua-melpa" (concat proto "://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/"))
+          (cons "tsinghua-marmalade" (concat proto "://mirrors.tuna.tsinghua.edu.cn/elpa/marmalade/"))
+          (cons "tsinghua-org" (concat proto "://mirrors.tuna.tsinghua.edu.cn/elpa/org/"))
+          ))
 
   ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
   ;; (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
