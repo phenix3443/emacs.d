66f91c81263e275d048b7a33ecb1a566a238bbcc
phenix3443 <phenix3443@gmail.com>
2020-03-05 23:46:52 +0800

    add new

---
+++ lisps/javascript-ide.el
@@ -0,0 +1,17 @@
+;;; package --- summary  javascript-ide配置
+;;; -*-coding:utf-8 -*-
+;;; commentary:
+
+;;; code:
+
+(use-package js-mode
+  ;; :ensure t
+  ;; :mode "\\.js$"
+  :hook (
+         (js-mode . format-all-mode)
+         (js-mode . lsp-deferred)
+         )
+  :config)
+
+(provide 'javascript-ide)
+;;; javascript-ide.el ends here
