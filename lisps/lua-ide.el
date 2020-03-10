6f7979e3cc80c5789fec3376fa258c72de7b7dc6
liushangliang <liushangliang@xunlei.com>
2020-03-10 17:10:15 +0800

    添加indent-line

--- lisps/lua-ide.el
+++ lisps/lua-ide.el
@@ -31,9 +31,12 @@
   :interpreter "lua"
   :hook (
          (lua-mode . set-company-backends-for-lua)
-         ;lsp 要在set-company-backends-for-lua后面，因为lsp会添加company-lsp到company-backends最前面，这样覆盖set-company-backends-for-lua 中设置的组合backends，这里也不要使用lsp-deferred，同样的原因。这样设置lsp会在set-company-bakcends函数前面
+         ;;lsp 要在set-company-backends-for-lua后面，因为lsp会添加company-lsp到company-backends最前面
+         ;;这样覆盖set-company-backends-for-lua 中设置的组合backends，这里也不要使用lsp-deferred
+         ;;同样的原因。这样设置lsp会在set-company-bakcends函数前面
          (lua-mode . lsp)
          (lua-mode . format-all-mode)
+         (lua-mode . highlight-indent-guides-mode)
          )
 
   :config
