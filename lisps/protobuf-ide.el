c1bf1550aec65a4066daee43dc7e1d1c9fa16e44
phenix3443 <phenix3443@gmail.com>
2020-09-05 11:47:40 +0800

    删除自定义的格式化

--- lisps/protobuf-ide.el
+++ lisps/protobuf-ide.el
@@ -8,12 +8,7 @@
   :mode ".proto$"
   :hook (protobuf-mode . untabify-buffer)
   :config
-  (defconst protobuf-style
-    '((c-basic-offset . 4)
-      (indent-tabs-mode . nil)))
-
-  (add-hook 'protobuf-mode-hook
-    (lambda () (c-add-style "my-style" protobuf-style t)))
+  ;; 使用clang-format格式化，不要使用模式自带的格式化功能
   )
 
 (provide 'protobuf-ide)
